5e571cec5d9d2fad4244f480d7dcfb021af7fdfd
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-03-26 12:10:38 +0000

    Ajout des scripts sql pour créer la base cultibox de l'interface + l'utilisateur cultibox + Ajout dans la doc du fichier de licence permettant de faire état de la licence des différents composants

---
+++ 01_software/database/cultibox.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le : Lun 26 Mars 2012 à 14:01
+-- Version du serveur: 5.5.16
+-- Version de PHP: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `cultibox`
+--
+CREATE DATABASE `cultibox` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `cultibox`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `configuration`
+--
+
+CREATE TABLE IF NOT EXISTS `configuration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `COLOR_HUMIDITY_GRAPH` varchar(30) NOT NULL DEFAULT 'green',
+  `COLOR_TEMPERATURE_GRAPH` varchar(30) NOT NULL DEFAULT 'red',
+  `RECORD_FREQUENCY` int(11) NOT NULL DEFAULT '5',
+  `NB_PLUGS` int(11) NOT NULL DEFAULT '4',
+  `UPDATE_PLUGS_FREQUENCY` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `configuration`
+--
+
+INSERT INTO `configuration` (`id`, `COLOR_HUMIDITY_GRAPH`, `COLOR_TEMPERATURE_GRAPH`, `RECORD_FREQUENCY`, `NB_PLUGS`, `UPDATE_PLUGS_FREQUENCY`) VALUES
+(1, 'green', 'red', 5, 4, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `logs`
+--
+
+CREATE TABLE IF NOT EXISTS `logs` (
+  `timestamp` varchar(14) NOT NULL DEFAULT '',
+  `temperature` int(11) DEFAULT NULL,
+  `humidity` int(11) DEFAULT NULL,
+  `date_catch` varchar(10) DEFAULT NULL,
+  `time_catch` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `logs`
+--
+
+INSERT INTO `logs` (`timestamp`, `temperature`, `humidity`, `date_catch`, `time_catch`) VALUES
+('12010103000000', 2307, 4098, '2012-01-01', '30000'),
+('12010103000500', 2133, 3641, '2012-01-01', '30005'),
+('12033103235500', 2320, 4368, '2012-03-31', '32355');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `plugs`
+--
+
+CREATE TABLE IF NOT EXISTS `plugs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
+
+--
+-- Contenu de la table `plugs`
+--
+
+INSERT INTO `plugs` (`id`, `name`) VALUES
+(1, 'plug1'),
+(2, 'plug2'),
+(3, 'plug3'),
+(4, 'plug4'),
+(5, 'plug5'),
+(6, 'plug6'),
+(7, 'plug7'),
+(8, 'plug8'),
+(9, 'plug9'),
+(10, 'plug10'),
+(11, 'plug11'),
+(12, 'plug12'),
+(13, 'plug13'),
+(14, 'plug14'),
+(15, 'plug15'),
+(16, 'plug16');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `programs`
+--
+
+CREATE TABLE IF NOT EXISTS `programs` (
+  `plug_id` int(11) NOT NULL,
+  `time_start` int(11) NOT NULL,
+  `time_stop` int(11) NOT NULL,
+  `value` int(11) NOT NULL,
+  `type` char(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
