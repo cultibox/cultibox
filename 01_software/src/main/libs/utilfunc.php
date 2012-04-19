8a3cf5ee0bc517a602134874ff338633a7b77262
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-04-19 13:58:40 +0000

    Debug de l'ajout de valeur sur une grande echelle + suppression des affichages de debug

--- 01_software/src/main/libs/utilfunc.php
+++ 01_software/src/main/libs/utilfunc.php
@@ -489,7 +489,11 @@ function format_program_highchart_data($arr) {
                                 $emm=substr($value[time_stop],2,2);
                                 $ess=substr($value[time_stop],4,2);
                                 $val_end=mktime($ehh,$emm,$ess,1,1,1970)*1000;
-                                $data="[".$val_start.",1],[".$val_end.",1],[".$val_end.",0]";
+				if(strcmp("$value[time_stop]","235959")==0) {
+                                	$data="[".$val_start.",1],[".$val_end.",1]";
+				} else {
+					$data="[".$val_start.",1],[".$val_end.",1],[".$val_end.",0]";
+				}
 			}
 
 			if((!empty($data))&&($value[value]!=0)&&(strcmp($value[time_start],"000000")!=0)) {
