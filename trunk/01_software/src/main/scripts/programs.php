09944761d64df8555da890a559b0f16199381722
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-04-27 10:11:13 +0000

    Multiple corrections et améliorations: ajout des valeurs et non seulement des on/off pour les prises, correction d'une partie de l'algo des programmes, correction du background des graphes, correction de typo, correction des messages d'erreur, ajout d'un bouton pour le timepicker, affichage des erreurs en dessous des programmes, conservation des heures rentrée par l'utilisateur une fois le formulaire soumis...

--- trunk/01_software/src/main/scripts/programs.php
+++ trunk/01_software/src/main/scripts/programs.php
@@ -15,16 +15,29 @@ $_SESSION['LANG'] = get_current_lang();
 __('LANG');
 
 $return="";
+$ret_plug=array();
 $info="";
 $nb_plugs=get_configuration("NB_PLUGS",$return);
 $selected_plug=getvar('selected_plug');
-$plugs_infos=get_plugs_names($nb_plugs,$return);
+$plugs_infos=get_plugs_infos($nb_plugs,$return);
+$exportid=getvar('exportid');
 
+if((isset($exportid))&&(!empty($exportid))) {
+	export_program($exportid);
+}
 
 
 if((!isset($sd_card))||(empty($sd_card))) {
 	$sd_card=get_sd_card();
 }
+
+
+if((!isset($sd_card))||(empty($sd_card))) {
+        $return=$return.__('ERROR_SD_CARD_CONF');
+} else {
+        $info=$info.__('INFO_SD_CARD').": $sd_card";
+}
+
 
 if(!empty($selected_plug)&&(isset($selected_plug))) { 
 	$start="start_time{$selected_plug}";
@@ -34,31 +47,44 @@ if(!empty($selected_plug)&&(isset($selected_plug))) {
 	$end_time=getvar($end);
 	$value_program=getvar($value);
 
-        if(("$start_time"!="")||("$end_time"!="")) {
-		if(check_times($start_time,$end_time,$return)) {
-			if((!empty($value_program))&&(isset($value_program))) {
+        if(("$start_time"!="")&&("$end_time"!="")) {
+		if(check_times($start_time,$end_time,$ret_plug[$selected_plug])) {
 				if("$value_program"=="on") $value_program="1";
 				if("$value_program"=="off") $value_program="0";
-				insert_program($selected_plug,$start_time,$end_time,$value_program,$return);
-			}
+				if(insert_program($selected_plug,$start_time,$end_time,$value_program,$ret_plug[$selected_plug])) {
+						$info_plug[$selected_plug]=$info_plug[$selected_plug].__(INFO_VALID_UPDATE_PROGRAM);
+				}
 		}
+	} else {
+		$ret_plug[$selected_plug]=$$ret_plug[$selected_plug].__('ERROR_MISSING_VALUE_TIME');
 	}
 }
 
-for($i=1;$i<=$nb_plugs;$i++) {
-	$data_plug=get_data_plug($i,$return);
-        $data{$i}=format_program_highchart_data($data_plug);
+for($i=0;$i<$nb_plugs;$i++) {
+	$data_plug=get_data_plug($i+1,$return);
+        $plugs_infos[$i][data]=format_program_highchart_data($data_plug);
+	switch($plugs_infos[$i][PLUG_TYPE]) {
+		case 'unknown': $plugs_infos[$i][translate]=__(PLUG_UNKNOWN);
+			break;
+		case 'ventilator': $plugs_infos[$i][translate]=__(PLUG_VENTILATOR);
+                        break;
+		case 'heating': $plugs_infos[$i][translate]=__(PLUG_HEATING);
+                        break;	
+		case 'lamp': $plugs_infos[$i][translate]=__(PLUG_LAMP);
+                        break;
+		case 'humidifier': $plugs_infos[$i][translate]=__(PLUG_HUMIDIFIER);
+                        break;
+                case 'dehumidifier': $plugs_infos[$i][translate]=__(PLUG_DESHUMIDIFIER);
+                        break;	
+	}
+
 }
 
-if((!isset($sd_card))||(empty($sd_card))) {
-        $return=$return.__('ERROR_SD_CARD_PROGRAMS');
-} else {
-        $info=$info.__('INFO_SD_CARD').": $sd_card";
+if((isset($sd_card))&&(!empty($sd_card))) {
+        $info=$info.__(INFO_SD_CARD).": $sd_card";
         $program=create_program_from_database();
         save_program_on_sd($sd_card,$program,$return,$info);
 }
-
-
 
 include('main/templates/programs.html');
 
