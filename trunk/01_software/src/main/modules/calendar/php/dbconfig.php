3851f8b3094c746ff673b6ac91867548a23f529c
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-05-08 20:42:56 +0000

    Modification de l'affichage des div d'erreur et d'information + modification de l'affichage des boutons du timepicker + ajout de la partie calendrier (qui sera à completer) + un peu de ménage

---
+++ trunk/01_software/src/main/modules/calendar/php/dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+class DBConnection{
+	function getConnection(){
+	  //change to your database server/user name/password
+		mysql_connect("localhost","cultibox","cultibox") or
+         die("Could not connect: " . mysql_error());
+    //change to your database name
+		mysql_select_db("cultibox") or 
+		     die("Could not select database: " . mysql_error());
+	}
+}
+?>
