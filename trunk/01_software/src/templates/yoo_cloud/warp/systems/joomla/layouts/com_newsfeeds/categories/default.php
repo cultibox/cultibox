b8c9fceef5ae957178f94503ae8ce6d9c87ba8f4
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-05-08 11:15:55 +0000

    Ajout du template yoo_cloud de Calagan dans le gestionnaire de version

---
+++ trunk/01_software/src/templates/yoo_cloud/warp/systems/joomla/layouts/com_newsfeeds/categories/default.php
@@ -0,0 +1,65 @@
+<?php
+/**
+* @package   Warp Theme Framework
+* @author    YOOtheme http://www.yootheme.com
+* @copyright Copyright (C) YOOtheme GmbH
+* @license   YOOtheme Proprietary Use License (http://www.yootheme.com/license)
+*/
+
+// no direct access
+defined('_JEXEC') or die;
+
+JHtml::addIncludePath(JPATH_COMPONENT.'/helpers');
+
+?>
+
+<div id="system">
+	
+	<?php if ($this->params->get('show_page_heading', 1)) : ?>
+	<h1 class="title"><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
+	<?php endif; ?>
+
+	<?php if ($this->params->get('show_base_description') && ($this->params->get('categories_description') || $this->parent->description)) : ?>
+	<div class="description">
+		<?php
+			if($this->params->get('categories_description')) {
+				echo  JHtml::_('content.prepare', $this->params->get('categories_description'), '', 'com_newsfeeds.categories');
+			} elseif ($this->parent->description) {
+				echo JHtml::_('content.prepare', $this->parent->description, '', 'com_newsfeeds.categories');
+			}
+		?>
+	</div>
+	<?php endif; ?>
+
+	<?php if (count($this->items[$this->parent->id]) > 0 && $this->maxLevelcat != 0) : ?>
+		<ul>
+		<?php foreach($this->items[$this->parent->id] as $id => $item) : ?>
+			<?php if ($this->params->get('show_empty_categories_cat') || $item->numitems || count($item->getChildren())) : ?>
+			<li>
+				<a href="<?php echo JRoute::_(NewsfeedsHelperRoute::getCategoryRoute($item->id));?>"><?php echo $this->escape($item->title); ?></a>
+				
+				<?php if ($this->params->get('show_cat_items_cat') == 1) : ?>
+				<small>(<?php echo $item->numitems; ?>)</small>
+				<?php endif; ?>
+
+				<?php if (($this->params->get('show_subcat_desc_cat') == 1) && $item->description) : ?>
+				<div><?php echo JHtml::_('content.prepare', $item->description, '', 'com_newsfeeds.categories'); ?></div>
+				<?php endif; ?>
+
+				<?php
+					if (count($item->getChildren()) > 0) {
+						$this->items[$item->id] = $item->getChildren();
+						$this->parent = $item;
+						$this->maxLevelcat--;
+						echo $this->loadTemplate('items');
+						$this->parent = $item->getParent();
+						$this->maxLevelcat++;
+					}
+				?>
+			</li>
+			<?php endif; ?>
+		<?php endforeach; ?>
+		</ul>
+	<?php endif; ?>
+
+</div>
