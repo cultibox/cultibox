190d588293a970e4fdfbb7eea51b207e5b07b038
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-05-22 11:19:59 +0000

    clean up du repertoire joomla avant réimportation

--- trunk/01_software/02_src/joomla/media/editors/tinymce/jscripts/tiny_mce/plugins/advhr/langs/fr_dlg.js
+++
@@ -1,8 +0,0 @@
-// FR lang variables - Sarki pour Joomla! FR 2011-07-17
-tinyMCE.addI18n('fr.advhr_dlg',{
-normal:"Normal",
-width:"Largeur",
-widthunits:"Unité",
-size:"Hauteur",
-noshade:"Sans ombre"
-});
