196c4adf6750debb81d97d9a2495196404640225
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-05-22 09:59:00 +0000

    Ajout de la partie joomla dans le dépot svn

---
+++ 01_software/02_src/joomla/logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2012-04-22 08:58:41 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2012-04-22	08:58:41	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2012-04-22	08:58:48	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2012-05-08	11:21:25	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2012-05-08	16:18:15	INFO	127.0.0.1	Joomla FAILURE: 	Empty password not allowed
