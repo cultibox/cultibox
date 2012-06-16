ce3ab2da51256e31899d83655e056c7fb15d9b63
alliaume.rico@gmail.com <alliaume.rico@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-06-16 10:44:05 +0000

    Mise à jour du script de lancement Mise à jour du fichier ISS suite à la revue avec YGe

---
+++ trunk/01_software/01_install/02_windows/cultibox.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+cd ..
+ECHO Demarage de l'application, veuillez patienter quelques secondes...
+xampp\xampp_start.exe
+start http://localhost:6891/cultibox/
+@ECHO OFF
+ECHO.
+ECHO Appuyez sur n'importe quelle touche pour terminer l'application...
+pause>Nul
+ECHO.
+ECHO Fermeture de l'application, veuillez patienter quelques secondes...
+xampp\xampp_stop>Nul
