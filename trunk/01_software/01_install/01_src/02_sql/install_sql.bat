381dfbbeef8e3cb1f95aca1422d0672e295cfec7
alliaume.rico@gmail.com <alliaume.rico@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-06-25 10:42:06 +0000

    -Ajout exec faux logs (t'aurai pu le faire, soit dit en passant...)

--- trunk/01_software/01_install/01_src/02_sql/install_sql.bat
+++ trunk/01_software/01_install/01_src/02_sql/install_sql.bat
@@ -4,3 +4,4 @@ xampp\mysql\bin\mysqladmin.exe -u root -h localhost password cultibox
 xampp\mysql\bin\mysql.exe -u root -h localhost -pcultibox < xampp\sql_install\user_cultibox.sql
 xampp\mysql\bin\mysql.exe -u root -h localhost -pcultibox < xampp\sql_install\joomla.sql
 xampp\mysql\bin\mysql.exe -u root -h localhost -pcultibox < xampp\sql_install\cultibox.sql
+xampp\mysql\bin\mysql.exe -u root -h localhost -pcultibox < xampp\sql_install\fake_log.sql
