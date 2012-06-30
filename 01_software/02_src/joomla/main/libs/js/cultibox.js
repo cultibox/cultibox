2f210a69a73ff50aae7925c0634abd8423985293
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-06-30 11:44:24 +0000

    Ajout de la gestion des infos bulles

---
+++ 01_software/02_src/joomla/main/libs/js/cultibox.js
@@ -0,0 +1,33 @@
+AfficherInfoBulle = function(e)
+{
+var text = $(this).next('.info-bulle-contenu');
+if (text.attr('class') != 'info-bulle-contenu')
+return false;
+text.fadeIn()
+.css('top', e.pageY)
+.css('left', e.pageX+10);
+return false;
+}CacherInfoBulle = function(e)
+{
+var text = $(this).next('.info-bulle-contenu');
+if (text.attr('class') != 'info-bulle-contenu')
+return false;
+text.fadeOut();
+}
+
+InstallationInfoBulle = function()
+{
+$('.info-bulle-css')
+.each(function(){
+$(this)
+.after($('<span/>')
+.attr('class', 'info-bulle-contenu')
+.html($(this).attr('title')))
+.attr('title', '');
+})
+.hover(AfficherInfoBulle, CacherInfoBulle);
+}
+
+$(document).ready(function() {
+InstallationInfoBulle();
+});
