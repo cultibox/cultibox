17aa82866e5ec654a2d4f0be1838bd784d640098
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-07-25 11:38:59 +0000

    Nouvelle version de la base pour la regulation secondaire + code pour la regulation secondaire (deuxième commit a venir)

--- trunk/01_software/01_install/01_src/02_sql/cultibox.sql
+++ trunk/01_software/01_install/01_src/02_sql/cultibox.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 25, 2012 at 03:00 PM
+-- Generation Time: Jul 25, 2012 at 01:36 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -43,8 +43,8 @@ CREATE TABLE IF NOT EXISTS `configuration` (
   `ALARM_ACTIV` varchar(4) NOT NULL DEFAULT '0000',
   `ALARM_VALUE` varchar(5) NOT NULL DEFAULT '50.00',
   `ALARM_SENSO` varchar(4) NOT NULL DEFAULT '000T',
-  `ALARM_SENSS` varchar(4) NOT NULL,
-  `FIRST_USE` varchar(5) NOT NULL DEFAULT 'True', 
+  `ALARM_SENSS` varchar(4) NOT NULL DEFAULT '000+',
+  `FIRST_USE` varchar(5) NOT NULL DEFAULT 'True',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -52,7 +52,8 @@ CREATE TABLE IF NOT EXISTS `configuration` (
 -- Dumping data for table `configuration`
 --
 
-INSERT INTO `configuration` (`id`, `COLOR_HUMIDITY_GRAPH`, `COLOR_TEMPERATURE_GRAPH`, `RECORD_FREQUENCY`, `POWER_FREQUENCY`, `NB_PLUGS`, `UPDATE_PLUGS_FREQUENCY`, `LANG`, `LOG_TEMP_AXIS`, `LOG_HYGRO_AXIS`, `SHOW_POPUP`, `ALARM_ACTIV`, `ALARM_VALUE`, `ALARM_SENSO`, `ALARM_SENSS`) VALUES (1, 'red', 'green', 5, 5, 3, -1, 'fr_FR', 50, 100, 'True', '0000', '50.0', '000T', '000+');
+INSERT INTO `configuration` (`id`, `COLOR_HUMIDITY_GRAPH`, `COLOR_TEMPERATURE_GRAPH`, `RECORD_FREQUENCY`, `POWER_FREQUENCY`, `NB_PLUGS`, `UPDATE_PLUGS_FREQUENCY`, `LANG`, `LOG_TEMP_AXIS`, `LOG_HYGRO_AXIS`, `SHOW_POPUP`, `ALARM_ACTIV`, `ALARM_VALUE`, `ALARM_SENSO`, `ALARM_SENSS`, `FIRST_USE`) VALUES
+(1, 'red', 'green', 5, 5, 16, -1, 'fr_FR', 50, 100, 'True', '0000', '50.0', '000H', '000+', 'True');
 
 -- --------------------------------------------------------
 
@@ -100,8 +101,12 @@ CREATE TABLE IF NOT EXISTS `plugs` (
   `PLUG_ID` varchar(3) DEFAULT NULL,
   `PLUG_NAME` varchar(30) DEFAULT NULL,
   `PLUG_TYPE` varchar(20) NOT NULL DEFAULT 'unknown',
-  `PLUG_TOLERANCE` decimal(3,1) DEFAULT 1.0,
+  `PLUG_TOLERANCE` decimal(3,1) DEFAULT NULL,
   `PLUG_POWER` int(11) NOT NULL DEFAULT '0',
+  `PLUG_REGUL` varchar(5) NOT NULL DEFAULT 'False',
+  `PLUG_SENSO` varchar(1) NOT NULL DEFAULT 'T',
+  `PLUG_SENSS` varchar(1) NOT NULL DEFAULT '+',
+  `PLUG_REGUL_VALUE` decimal(3,1) NOT NULL DEFAULT '35.0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
 
@@ -109,23 +114,23 @@ CREATE TABLE IF NOT EXISTS `plugs` (
 -- Dumping data for table `plugs`
 --
 
-INSERT INTO `plugs` (`id`, `PLUG_ID`, `PLUG_NAME`, `PLUG_TYPE`, `PLUG_TOLERANCE`, `PLUG_POWER`) VALUES
-(1, '', 'HPS/MH', 'lamp', 1.0, 0),
-(2, '', 'plug2', 'unknown', 1.0, 0),
-(3, '', 'plug3', 'unknown', 1.0, 0),
-(4, '', 'plug4', 'unknown', 1.0, 0),
-(5, '', 'plug5', 'unknown', 1.0, 0),
-(6, '', 'plug6', 'unknown', 1.0, 0),
-(7, '', 'plug7', 'unknown', 1.0, 0),
-(8, '', 'plug8', 'unknown', 1.0, 0),
-(9, '', 'plug9', 'unknown', 1.0, 0),
-(10, '', 'plug10', 'unknown', 1.0, 0),
-(11, '', 'plug11', 'unknown', 1.0, 0),
-(12, '', 'plug12', 'unknown', 1.0, 0),
-(13, '', 'plug13', 'unknown', 1.0, 0),
-(14, '', 'plug14', 'unknown', 1.0, 0),
-(15, '', 'plug15', 'unknown', 1.0, 0),
-(16, '', 'plug16', 'unknown', 1.0, 0);
+INSERT INTO `plugs` (`id`, `PLUG_ID`, `PLUG_NAME`, `PLUG_TYPE`, `PLUG_TOLERANCE`, `PLUG_POWER`, `PLUG_REGUL`, `PLUG_SENSO`, `PLUG_SENSS`, `PLUG_REGUL_VALUE`) VALUES
+(1, '', 'plug1', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(2, '', 'plug2', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(3, '', 'plug3', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(4, '219', 'plug4', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(5, '215', 'plug5', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(6, '207', 'plug6', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(7, '190', 'plug7', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(8, '189', 'plug8', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(9, '187', 'plug9', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(10, '183', 'plug10', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(11, '175', 'plug11', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(12, '126', 'plug12', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(13, '123', 'plug13', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(14, '123', 'plug14', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(15, '123', 'plug15', 'unknown', NULL, 0, 'False', 'T', '+', 35.0),
+(16, '123', 'plug16', 'unknown', NULL, 0, 'False', 'T', '+', 35.0);
 
 -- --------------------------------------------------------
 
