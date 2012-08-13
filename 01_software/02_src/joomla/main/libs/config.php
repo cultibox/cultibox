dc22031fa180fadb054279615f310312bbb62d89
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-08-13 14:42:33 +0000

    Un peu de ménage

--- 01_software/02_src/joomla/main/libs/config.php
+++ 01_software/02_src/joomla/main/libs/config.php
@@ -3,22 +3,22 @@
 //	This configuration file is dedicated to some GLOBALS variables 
 //	to define some possible customizations	
 
-// Default language
+// Default language:
 define('LANG_FALLBACK', 'en_GB');
 
 // Default port use by the application:
 $GLOBALS['SOFT_PORT'] = '6891';
 
-// List of available languages (l10N format)
+// List of available languages: (l10N format)
 $GLOBALS['LIST_LANG'] = array('en_GB','fr_FR');
 
-// List of available colors for graphics
+// List of available colors for graphics:
 $GLOBALS['LIST_GRAPHIC_COLOR'] = array('blue','red', 'green', 'black');
 
 // List of the recording frequency available: (in minute)
 $GLOBALS['LIST_RECORD_FREQUENCY'] = array('1','5','30');
 
-// List of plugs available
+// List of plugs available:
 $GLOBALS['LIST_NB_PLUGS'] = array('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16');
 
 // List of the updating plugs frequency: (-1 means every clock heartbeat)
@@ -27,7 +27,7 @@ $GLOBALS['LIST_UPDATE_FREQUENCY'] = array('-1','1','5');
 // Colors for plugs program:
 $GLOBALS['LIST_GRAPHIC_COLOR_PROGRAM'] = array('blue','red', 'green', 'black','purple','pink','yellow','brown','grey','orange','violet','beige','turquoise','amber','cyan','indigo');
 
-//Subject for the calendar
+//Subject for the calendar:
 $GLOBALS['LIST_SUBJECT_CALENDAR'] = array('Beginning','Fertilizers', 'Water', 'Bloom','Harvest','Other');
 
 //To print debug trace:
@@ -39,7 +39,7 @@ $GLOBALS['PLUGA_DEFAULT'] = array('004', '247', '222', '219', '215', '207', '190
 // List of value possible for temperature and hygrometry axis:
 $GLOBALS['LIST_MAX_AXIS'] = array('20', '40', '50', '80','100');
 
-// Variable to enable/disable "first use" page
+// Variable to enable/disable "first use" page:
 $GLOBALS['FIRST_USE'] = true;
 
 ?>
