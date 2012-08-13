2c3829a0d2df7b5313e73d8aa64a154f9e5a02f6
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-08-13 14:42:33 +0000

    Un peu de ménage

--- trunk/01_software/02_src/joomla/main/libs/js/cultibox.js
+++ trunk/01_software/02_src/joomla/main/libs/js/cultibox.js
@@ -1,33 +1,30 @@
-AfficherInfoBulle = function(e)
-{
-var text = $(this).next('.info-bulle-contenu');
-if (text.attr('class') != 'info-bulle-contenu')
-return false;
-text.fadeIn()
-.css('top', e.pageY)
-.css('left', e.pageX+10);
-return false;
-}CacherInfoBulle = function(e)
-{
-var text = $(this).next('.info-bulle-contenu');
-if (text.attr('class') != 'info-bulle-contenu')
-return false;
-text.fadeOut();
+AfficherInfoBulle = function(e) {
+   var text = $(this).next('.info-bulle-contenu');
+   if (text.attr('class') != 'info-bulle-contenu') return false;
+   text.fadeIn()
+   .css('top', e.pageY)
+   .css('left', e.pageX+10);
+   return false;
 }
 
-InstallationInfoBulle = function()
-{
-$('.info-bulle-css')
-.each(function(){
-$(this)
-.after($('<span/>')
-.attr('class', 'info-bulle-contenu')
-.html($(this).attr('title')))
-.attr('title', '');
-})
-.hover(AfficherInfoBulle, CacherInfoBulle);
+CacherInfoBulle = function(e) {
+   var text = $(this).next('.info-bulle-contenu');
+   if (text.attr('class') != 'info-bulle-contenu') return false;
+   text.fadeOut();
+}
+
+InstallationInfoBulle = function() {
+   $('.info-bulle-css')
+   .each(function(){
+      $(this)
+      .after($('<span/>')
+      .attr('class', 'info-bulle-contenu')
+      .html($(this).attr('title')))
+      .attr('title', '');
+   })
+   .hover(AfficherInfoBulle, CacherInfoBulle);
 }
 
 $(document).ready(function() {
-InstallationInfoBulle();
+   InstallationInfoBulle();
 });
