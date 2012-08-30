09249b0403863f02acbb42a7baef182814fef570
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-08-30 15:55:00 +0000

    Ajout des fichiers pour la création des packages Ubuntu

---
+++ 01_software/01_install/03_linux/pre-compil.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e 
+case "$1" in
+      "ubuntu-precise64" )
+           dir=`dirname $0`
+           cd $dir
+           tar xzvf xampp-linux-1.7.7.tar.gz -C ubuntu-precise64/
+           rm ubuntu-precise64/lampp/share/man
+           rm ubuntu-precise64/lampp/share/openssl/man
+           cp -R ../../02_src/joomla ubuntu-precise64/lampp/htdocs/cultibox
+           cp conf-lampp/config.inc.php ubuntu-precise64/lampp/etc/
+           cp conf-lampp/httpd.conf ubuntu-precise64/lampp/etc/
+           cp conf-lampp/php.ini ubuntu-precise64/lampp/etc/
+           cp conf-lampp/config.inc.php ubuntu-precise64/lampp/phpmyadmin/
+           cp debreate-package/takecontrol.png ubuntu-precise64/lampp/
+           cp -R ../../01_install/01_src/03_sd ubuntu-precise64/lampp/sd
+           cp -R ../../01_install/01_src/02_sql ubuntu-precise64/lampp/sql_install                 
+           cp -R daemon ubuntu-precise64/lampp/
+           find ./ubuntu-precise64/lampp -name ".svn"|xargs rm -Rf
+      ;; 
+      "clean")
+            rm -Rf ubuntu-precise64/lampp
+
+      ;;
+      *)
+            echo "usage: $0"
+            echo "                      ubuntu-precise64"
+            echo "                      clean"
+      ;;
+esac
