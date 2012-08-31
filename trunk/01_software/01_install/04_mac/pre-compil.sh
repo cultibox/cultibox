bb2290127b4547741b06adacfbe8b9a27fe9f375
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-08-31 16:31:14 +0000

    Ajout des fichiers pour packages Mac Os X

---
+++ trunk/01_software/01_install/04_mac/pre-compil.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e 
+case "$1" in
+      "snow-leopard" )
+           dir=`dirname $0`
+           cd $dir
+           cp -R XAMPP snow-leopard/
+           cp -R ../../02_src/joomla snow-leopard/XAMPP/htdocs/cultibox
+           cp conf-lampp/config.inc.php snow-leopard/XAMPP/xamppfiles/phpmyadmin/
+           cp conf-lampp/httpd.conf snow-leopard/XAMPP/etc/
+           cp conf-lampp/php.ini snow-leopard/XAMPP/etc/
+           cp packagemaker/takecontrol.png snow-leopard/XAMPP/
+           cp -R ../../01_install/01_src/03_sd snow-leopard/XAMPP/sd
+           cp -R ../../01_install/01_src/02_sql snow-leopard/XAMPP/sql_install                 
+           find ./snow-leopard/XAMPP/ -name ".svn"|xargs rm -Rf
+           cd snow-leopard && tar zcvf cultibox.tar.gz ./snow-leopard/XAMPP
+      ;; 
+      "clean")
+            rm -Rf snow-leopard/XAMPP
+
+      ;;
+      *)
+            echo "usage: $0"
+            echo "                      snow-leopard"
+            echo "                      clean"
+      ;;
+esac
