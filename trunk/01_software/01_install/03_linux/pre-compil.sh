e66c8fb6063ce7f30630eebba336e6258b723b7c
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-09-02 09:25:29 +0000

    Mise en place de l'archi pour les packges ubuntu 32 bits

--- trunk/01_software/01_install/03_linux/pre-compil.sh
+++ trunk/01_software/01_install/03_linux/pre-compil.sh
@@ -14,19 +14,45 @@ case "$1" in
            cp conf-lampp/php.ini ubuntu-precise64/lampp/etc/
            cp conf-lampp/config.inc.php ubuntu-precise64/lampp/phpmyadmin/
            cp conf-lampp/httpd-xampp.conf ubuntu-precise64/lampp/etc/extra/
+           cp conf-lampp/my.cnf ubuntu-precise64/lampp/etc/
            cp debreate-package/takecontrol.png ubuntu-precise64/lampp/
            cp -R ../../01_install/01_src/03_sd ubuntu-precise64/lampp/sd
            cp -R ../../01_install/01_src/02_sql ubuntu-precise64/lampp/sql_install                 
            cp -R daemon ubuntu-precise64/lampp/
            find ./ubuntu-precise64/lampp -name ".svn"|xargs rm -Rf
+           debreate debreate-package/cultibox_amd64.dbp
       ;; 
+      "ubuntu-precise32")
+           dir=`dirname $0`
+           cd $dir
+           tar xzvf xampp-linux-lite-1.7.7.tar.gz -C ubuntu-precise32/
+           rm ubuntu-precise32/lampp/share/man
+           rm ubuntu-precise32/lampp/share/openssl/man
+           rm ubuntu-precise32/lampp/lib/terminfo
+           cp -R ../../02_src/joomla ubuntu-precise32/lampp/htdocs/cultibox
+           cp conf-lampp/httpd.conf ubuntu-precise32/lampp/etc/
+           cp conf-lampp/php.ini ubuntu-precise32/lampp/etc/
+           cp conf-lampp/config.inc.php ubuntu-precise32/lampp/phpmyadmin/
+           cp conf-lampp/httpd-xampp.conf ubuntu-precise32/lampp/etc/extra/
+           cp conf-lampp/my.cnf ubuntu-precise32/lampp/etc/
+           cp debreate-package/takecontrol.png ubuntu-precise32/lampp/
+           cp -R ../../01_install/01_src/03_sd ubuntu-precise32/lampp/sd
+           cp -R ../../01_install/01_src/02_sql ubuntu-precise32/lampp/sql_install
+           cp -R daemon ubuntu-precise32/lampp/
+           find ./ubuntu-precise32/lampp -name ".svn"|xargs rm -Rf
+           debreate debreate-package/cultibox_i386.dbp
+
+
+      ;;
       "clean")
             rm -Rf ubuntu-precise64/lampp
+            rm -Rf ubuntu-precise32/lampp
 
       ;;
       *)
             echo "usage: $0"
             echo "                      ubuntu-precise64"
+            echo "                      ubuntu-precise32"
             echo "                      clean"
       ;;
 esac
