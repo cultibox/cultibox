904e06159c686ea7a846db151f76d07c0e5ec9de
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-09-13 08:40:38 +0000

    Valeur par défaut du prix du kWh

--- 01_software/01_install/01_src/02_sql/cultibox.sql
+++ 01_software/01_install/01_src/02_sql/cultibox.sql
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `configuration` (
   `ALARM_VALUE` varchar(5) NOT NULL DEFAULT '50.00',
   `ALARM_SENSO` varchar(4) NOT NULL DEFAULT '000T',
   `ALARM_SENSS` varchar(4) NOT NULL DEFAULT '000+',
-  `COST_PRICE` decimal(3,2) NOT NULL DEFAULT '0.00',
+  `COST_PRICE` decimal(6,4) NOT NULL DEFAULT '0.1249',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `configuration` (
 --
 
 INSERT INTO `configuration` (`id`, `CHECK_UPDATE`, `VERSION`, `COLOR_HUMIDITY_GRAPH`, `COLOR_TEMPERATURE_GRAPH`, `COLOR_POWER_GRAPH`, `COLOR_PROGRAM_GRAPH`, `COLOR_COST_GRAPH`, `RECORD_FREQUENCY`, `POWER_FREQUENCY`, `NB_PLUGS`, `UPDATE_PLUGS_FREQUENCY`, `LANG`, `LOG_TEMP_AXIS`, `LOG_HYGRO_AXIS`, `LOG_POWER_AXIS`, `SHOW_POPUP`, `ALARM_ACTIV`, `ALARM_VALUE`, `ALARM_SENSO`, `ALARM_SENSS`, `COST_PRICE`) VALUES
-(1, 'False', '1.0.0', 'red', 'green', 'black', 'blue', 'purple', 5, 1, 3, -1, 'fr_FR', 50, 100, 1000, 'True', '0000', '50.0', '000H', '000+', 0.00);
+(1, 'False', '1.0.0', 'red', 'green', 'black', 'blue', 'purple', 5, 1, 3, -1, 'fr_FR', 50, 100, 1000, 'True', '0000', '50.0', '000H', '000+', 0.1249);
 
 -- --------------------------------------------------------
 
