6c29070ee062049e11c5e70e4a30d65ab0b7c711
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-10-26 13:13:22 +0000

    Nouvelle recette pour la création des packages Mac

--- trunk/01_software/01_install/04_mac/pre-compil.sh
+++ trunk/01_software/01_install/04_mac/pre-compil.sh
@@ -5,16 +5,16 @@ case "$1" in
       "snow-leopard" )
            dir=`dirname $0`
            cd $dir
-           cp -R XAMPP snow-leopard/
+           tar pzxvf xampp-mac-lite-1.7.7.tar.gz -C snow-leopard/
            cp -R ../../02_src/joomla snow-leopard/XAMPP/htdocs/cultibox
-           cp conf-lampp/config.inc.php snow-leopard/XAMPP/xamppfiles/phpmyadmin/
+           #cp conf-lampp/config.inc.php snow-leopard/XAMPP/xamppfiles/phpmyadmin/
            cp conf-lampp/httpd.conf snow-leopard/XAMPP/etc/
            cp conf-lampp/php.ini snow-leopard/XAMPP/etc/
            cp packagemaker/takecontrol.png snow-leopard/XAMPP/
            cp -R ../../01_install/01_src/03_sd snow-leopard/XAMPP/sd
            cp -R ../../01_install/01_src/02_sql snow-leopard/XAMPP/sql_install                 
            find ./snow-leopard/XAMPP/ -name ".svn"|xargs rm -Rf
-           cd snow-leopard && tar zcvf cultibox.tar.gz ./snow-leopard/XAMPP
+           cd snow-leopard && tar pzcvf cultibox.mac.tar.gz XAMPP
       ;; 
       "clean")
             rm -Rf snow-leopard/XAMPP
