6fe7a8999b23575dbfbadba1d7b3ed8d80e72d66
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-10-29 09:26:57 +0000

    Le fichier de démarrage cultibox.bat pour windows detecte maintenant si l'interface est déja lancé (via les ports utilisés) et la redémarre si c'est le cas + ajout du fichier ISS permettant de faire le build sous linux

--- 01_software/01_install/02_windows/cultibox.bat
+++ 01_software/01_install/02_windows/cultibox.bat
@@ -1,11 +1,23 @@
 @ECHO OFF
-ECHO Demarage de l'application, veuillez patienter quelques secondes...
-xampp\xampp_start.exe
+ECHO * Demarage de l'application, veuillez patienter quelques secondes...
+netstat -an|find ":6891"|find "LISTENING" > NUL
+if %ERRORLEVEL% EQU 0 (
+    echo    XAMPP semble deja demarre, redemarrage de XAMPP:
+    echo         Arret de XAMPP...
+    xampp\xampp_stop.exe
+    echo         Demarrage de XAMPP
+    xampp\xampp_start.exe > NUL
+) else (
+    echo      -> Demarrage de XAMPP
+    xampp\xampp_start.exe > NUL
+)
+
+echo * Demarrage de l'interface...
 start http://localhost:6891/cultibox/
 @ECHO OFF
 ECHO.
-ECHO Appuyez sur n'importe quelle touche pour terminer l'application...
+ECHO * Appuyez sur n'importe quelle touche pour terminer l'application...
 pause>Nul
 ECHO.
-ECHO Fermeture de l'application, veuillez patienter quelques secondes...
-xampp\xampp_stop>Nul
+ECHO * Fermeture de l'application, veuillez patienter quelques secondes...
+xampp\xampp_stop.exe
