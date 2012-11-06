d687d4e3cb950114e3e3b9dfb4f7900ac760cd7c
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-11-06 11:37:34 +0000

    Gestion des paquets automatiques pour Windows et gestion des mises à jours, enfin...poil aux mains

--- 01_software/01_install/02_windows/load.bat
+++ 01_software/01_install/02_windows/load.bat
@@ -1,25 +1,7 @@
-if exist ..\backup\logs (
-copy ..\backup\logs ..\xampp\mysql\data\logs
-..\xampp\mysql\bin\mysql.exe cultibox -u root -h localhost -pcultibox -e "DELETE FROM logs"
-..\xampp\mysql\bin\mysql.exe cultibox -u root -h localhost -pcultibox -e "LOAD DATA INFILE './logs' REPLACE INTO TABLE logs FIELDS TERMINATED BY ';' IGNORE 1 LINES"
-del ..\backup\logs
-del  ..\xampp\mysql\data\logs
-)
-if exist ..\backup\plugs (
-copy ..\backup\plugs ..\xampp\mysql\data\plugs
-..\xampp\mysql\bin\mysql.exe cultibox -u root -h localhost -pcultibox -e "LOAD DATA INFILE './plugs' REPLACE INTO TABLE plugs FIELDS TERMINATED BY '\t' IGNORE 1 LINES"
-del ..\backup\plugs
-del  ..\xampp\mysql\data\plugs
-)
-if exist ..\backup\programs (
-copy ..\backup\programs ..\xampp\mysql\data\programs
-..\xampp\mysql\bin\mysql.exe cultibox -u root -h localhost -pcultibox -e "LOAD DATA INFILE './programs' REPLACE INTO TABLE programs FIELDS TERMINATED BY ';' IGNORE 1 LINES"
-del ..\backup\programs
-del  ..\xampp\mysql\data\programs
-)
-if exist ..\backup\configuration (
-copy ..\backup\configuration ..\xampp\mysql\data\configuration
-..\xampp\mysql\bin\mysql.exe cultibox -u root -h localhost -pcultibox -e "LOAD DATA INFILE './configuration' REPLACE INTO TABLE configuration FIELDS TERMINATED BY ';' IGNORE 1 LINES"
-del ..\backup\configuration
-del  ..\xampp\mysql\data\configuration
-)
+xampp\mysql\bin\mysql.exe -u root -h localhost -pcultibox cultibox -e "DELETE FROM logs"
+xampp\mysql\bin\mysql.exe -u root -h localhost -pcultibox cultibox -e "DELETE FROM configuration"
+xampp\mysql\bin\mysql.exe -u root -h localhost -pcultibox cultibox -e "DELETE FROM plugs"
+xampp\mysql\bin\mysql.exe -u root -h localhost -pcultibox cultibox -e "DELETE FROM programs"
+xampp\mysql\bin\mysql.exe -u root -h localhost -pcultibox cultibox -e "DELETE FROM jqcalendar"
+xampp\mysql\bin\mysql.exe -u root -h localhost -pcultibox cultibox -e "DELETE FROM power"
+xampp\mysql\bin\mysql.exe -u root -h localhost -pcultibox cultibox < ./backup/backup.sql
