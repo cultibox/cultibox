3c5eefa6e22e918a5a4714bdda4e01a8b7a88341
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-11-06 11:37:34 +0000

    Gestion des paquets automatiques pour Windows et gestion des mises à jours, enfin...poil aux mains

--- trunk/01_software/01_install/02_windows/backup.bat
+++ trunk/01_software/01_install/02_windows/backup.bat
@@ -1,6 +1,2 @@
 mkdir backup
-xampp\mysql\bin\mysql.exe  cultibox -u root -h localhost -pcultibox -e "SELECT CONCAT(timestamp,';',temperature,';',humidity,';',date_catch,';',time_catch,';',fake_log) FROM `logs`" > ./backup/logs
-xampp\mysql\bin\mysql.exe  cultibox -u root -h localhost -pcultibox -e "SELECT * FROM `plugs`" > ./backup/plugs
-xampp\mysql\bin\mysql.exe  cultibox -u root -h localhost -pcultibox -e "SELECT CONCAT(plug_id,';',time_start,';',time_stop,';',value) FROM `programs`" > ./backup/programs
-xampp\mysql\bin\mysql.exe  cultibox -u root -h localhost -pcultibox -e "SELECT CONCAT(id,';',COLOR_HUMIDITY_GRAPH,';',COLOR_TEMPERATURE_GRAPH,';',RECORD_FREQUENCY,';',POWER_FREQUENCY,';',NB_PLUGS,';',UPDATE_PLUGS_FREQUENCY,';',LANG,';',LOG_TEMP_AXIS,';',LOG_HYGRO_AXIS,';',SHOW_POPUP,';',ALARM_ACTIV,';',ALARM_VALUE,';',ALARM_SENSO,';',ALARM_SENSS,';',FIRST_USE) FROM `configuration`" > ./backup/configuration
-pause
+xampp\mysql\bin\mysqldump.exe --no-create-db --no-create-info -u root -h localhost -pcultibox cultibox jqcalendar logs configuration plugs power programs > ./backup/backup.sql
--- trunk/01_software/01_install/02_windows/pre_compil_linux.sh
+++ trunk/01_software/01_install/02_windows/pre_compil_linux.sh
@@ -2,21 +2,111 @@
 
 set -e 
 VERSION=`cat ../../VERSION`
+SRC_DIR=../../02_src/joomla
+DEST_DIR=../../01_install/01_src/01_xampp
+
 
 case "$1" in
       "windows7" )
-            sed -i "s/#define MyAppVersion .*/#define MyAppVersion \"`echo $VERSION`\"/" ./install_script_linux.iss
+            dir=`dirname $0`
+            cd $dir
+            sudo rm -Rf ../01_src/01_xampp/*
+            sed -i "s/#define MyAppVersion .*/#define MyAppVersion \"`echo $VERSION`\"/" ./install_script.iss
+            sed -i "s/OutputBaseFilename=.*/OutputBaseFilename=setup_cultibox_{#MyAppVersion}/" ./install_script.iss
             sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`'/" ../../01_install/01_src/02_sql/cultibox.sql
             tar zxvf xamp-lite-windows-1.7.7.tar.gz -C ../01_src/01_xampp/
             cp -R ../../02_src/joomla ../01_src/01_xampp/htdocs/cultibox
-            wine "C:\Program Files (x86)\Inno Setup 5\iscc.exe"  "install_script_linux.iss"
+            echo "### Don't delete this file ###" > ../01_src/01_xampp/VERSION_`echo $VERSION`.txt
+            echo "### Ne pas supprimer ce fichier ### " >> ../01_src/01_xampp/VERSION_`echo $VERSION`.txt
+            echo "" >> ../01_src/01_xampp/VERSION_`echo $VERSION`.txt
+            cat ../../CHANGELOG >> ../01_src/01_xampp/VERSION_`echo $VERSION`.txt
+            wine "C:\Program Files (x86)\Inno Setup 5\iscc.exe"  "install_script.iss"
       ;;
       "windows7-admin" )
-            sed -i "s/#define MyAppVersion .*/#define MyAppVersion \"`echo $VERSION`\"/" ./install_script_linux.iss
+            dir=`dirname $0`
+            cd $dir
+            sudo rm -Rf ../01_src/01_xampp/*
+            sed -i "s/#define MyAppVersion .*/#define MyAppVersion \"`echo $VERSION`\"/" ./install_script.iss
             sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`'/" ../../01_install/01_src/02_sql/cultibox.sql
-            tar zxvf xamp-lite-windows-admin-1.7.7.tar.gz -C ../01_src/01_xampp/
+            sed -i "s/OutputBaseFilename=.*/OutputBaseFilename=setup_cultibox_admin_{#MyAppVersion}/" ./install_script.iss 
+            tar zxvf xamp-lite-admin-windows-1.7.7.tar.gz -C ../01_src/01_xampp/
             cp -R ../../02_src/joomla ../01_src/01_xampp/htdocs/cultibox
-            wine "C:\Program Files (x86)\Inno Setup 5\iscc.exe"  "install_script_linux.iss"
+            echo "### Don't delete this file ###" > ../01_src/01_xampp/VERSION_`echo $VERSION`.txt
+            echo "### Ne pas supprimer ce fichier ### " >> ../01_src/01_xampp/VERSION_`echo $VERSION`.txt
+            echo "" >> ../01_src/01_xampp/VERSION_`echo $VERSION`.txt
+            cat ../../CHANGELOG >> ../01_src/01_xampp/VERSION_`echo $VERSION`.txt
+            wine "C:\Program Files (x86)\Inno Setup 5\iscc.exe"  "install_script.iss"
+      ;;
+      "update" )
+            if [ "$2" == "" ] || [ "$3" == "" ]; then
+                    $0 
+                    exit 0
+            fi
+            
+            dir=`dirname $0`
+            cd $dir
+            sudo rm -Rf ../01_src/01_xampp/*
+            sed -i "s/#define MyAppVersion .*/#define MyAppVersion \"`echo $3`\"/" ./update_script_linux.iss
+            sed -i "s/#define MyOldAppVersion .*/#define MyOldAppVersion \"`echo $2`\"/" ./update_script_linux.iss
+
+            file_section=`grep -n "\[Files\]" ./update_script_linux.iss |awk -F":" '{print $1}'`
+            run_section=`grep -n "\[Run\]" ./update_script_linux.iss |awk -F":" '{print $1}'`
+            
+            if [ "$file_section" == "" ] || [ "$run_section" == "" ]; then
+                    echo "[ Error ] - Please check the update script (update_script_linux.iss): missing [Files] or [Run] section(s)"
+                    exit 0
+            fi
+
+            file_section=`expr $file_section + 7`
+            run_section=`expr $run_section - 1`
+
+
+            if [ $run_section -gt $file_section ]; then
+                sed -i "${file_section},${run_section}d" ./update_script_linux.iss
+            fi
+ 
+            mkdir -p $DEST_DIR/htdocs/cultibox
+            copy_nb=0
+            while read line; do
+                    copy=`echo $line|awk -F":" '{print $1}'`
+                    file=`echo $line|awk -F":" '{print $2}'`
+                    dir_file=`dirname $file`
+
+                    if [ "$copy" == "SRC" ]; then
+
+                        bdir=`dirname $file`
+                        mkdir -p $DEST_DIR/htdocs/cultibox/$bdir
+                        cp $SRC_DIR/$file  $DEST_DIR/htdocs/cultibox/$bdir/
+                        copy_nb=`expr $copy_nb + 1`
+
+                        tmp_file=`echo "$file" | sed -e 's/.*/\/&/'` 
+                        line_toadd=`echo "$tmp_file" | sed "s#/#\\\\\\\\\\\\\#g"`
+                        tmp_dir=`echo "$dir_file" | sed -e 's/.*/\/&/'`
+                        dir_toadd=`echo "$tmp_dir" | sed "s#/#\\\\\\\\\\\\\#g"`
+                        sed -i "${file_section}i\Source: \"C:\\\users\\\yann\\\Desktop\\\Project\\\cultibox\\\01_software\\\02_src\\\joomla${line_toadd}\"; DestDir: \"{app}\\\xampp\\\htdocs\\\cultibox${dir_toadd}\\\\\"; CopyMode: alwaysoverwrite; Flags: ignoreversion" ./update_script_linux.iss
+                        file_section=`expr $file_section + 1`
+
+                        
+
+                    fi
+            done < ./update_file/update_windows_from_$2_to_$3
+
+            echo "SRC files: $copy_nb";
+
+            if [ ! -f ../01_src/02_sql/sql_update/update_sql_windows_from_$2_to_$3.sql ]; then
+                touch ../01_src/01_xampp/update_sql_windows_from_$2_to_$3.sql
+            else
+                cp ../01_src/02_sql/sql_update/update_sql_windows_from_$2_to_$3.sql ../01_src/01_xampp/
+            fi
+
+            echo "### Don't delete this file ###" > ../01_src/01_xampp/VERSION_`echo $3`.txt
+            echo "### Ne pas supprimer ce fichier ### " >> ../01_src/01_xampp/VERSION_`echo $3`.txt
+            echo "" >> ../01_src/01_xampp/VERSION_`echo $3`.txt
+            cat ../../CHANGELOG >> ../01_src/01_xampp/VERSION_`echo $3`.txt
+
+            sed -i "${file_section}i\Source: \"C:\\\users\\\yann\\\Desktop\\\Project\\\cultibox\\\01_software\\\01_install\\\01_src\\\01_xampp\\\VERSION_${3}.txt\"; DestDir: \"{app}\\\xampp\"; Flags: ignoreversion" ./update_script_linux.iss
+            echo "UPDATE \`configuration\` SET \`VERSION\` = '${3}' WHERE \`configuration\`.\`id\` =1;" >> ../01_src/01_xampp/update_sql_windows_from_$2_to_$3.sql
+            wine "C:\Program Files (x86)\Inno Setup 5\iscc.exe"  "update_script_linux.iss"
       ;;
       "clean")
             sudo rm -Rf ../01_src/01_xampp/*
@@ -25,6 +115,7 @@ case "$1" in
             echo "usage: $0"
             echo "                      windows7"
             echo "                      windows7-admin"
+            echo "                      update <old_version> <new_version>"
             echo "                      clean"
       ;;
 esac
