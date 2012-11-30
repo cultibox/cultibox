2581183b2277d745f8cd48a6b5337f15d74f4481
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2012-11-30 09:45:51 +0000

    Ajout de la version du soft dans xhr + changelent de l'affichage de la configuration

--- 01_software/02_src/joomla/main/libs/js/oXHR.js
+++ 01_software/02_src/joomla/main/libs/js/oXHR.js
@@ -28,12 +28,13 @@ function getXMLHttpRequest() {
 
 
 
-function sendXMLHttpRequest(xhr,infos) {
+function sendXMLHttpRequest(xhr,value_os,value_version) {
     var sDate = encodeURIComponent(new Date());
     var sLog = encodeURIComponent("pouet");
-    var sIP = encodeURIComponent(infos);
+    var sIP = encodeURIComponent(value_os);
+    var sVersion = encodeURIComponent(value_version);
 
     //xhr.timeout = 4000;
-    xhr.open("GET", "http://www.cbx.greenbox-botanic.com/index.php?date=" + sDate + "&log=" + sLog + "&ip=" + sIP, true);
+    xhr.open("GET", "http://www.cbx.greenbox-botanic.com/index.php?date=" + sDate + "&log=" + sLog + "&ip=" + sIP + "&cbx_soft_version=" + sVersion, true);
     xhr.send(null);
 }
