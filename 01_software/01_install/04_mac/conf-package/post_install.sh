fc2bd7cfd1e1372617c13419763a638447f640e7
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-03-19 15:04:40 +0000

    Nouvelle image pour l'install mac + nouvelle conf interface

--- 01_software/01_install/04_mac/conf-package/post_install.sh
+++ 01_software/01_install/04_mac/conf-package/post_install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "Configuring your CultiBox environment:"
+ln -s /Applications/cultibox /Applications/XAMPP
 user_culti=`who|head -1|awk -F" " '{print $1}'`
 group_culti=`who|head -1|awk -F" " '{print $1}'|xargs id -gn`
 
-chown -R $user_culti:$group_culti /Applications/XAMPP
-sed -i '' "s/User nobody/User $user_culti/" /Applications/XAMPP/xamppfiles/etc/httpd.conf
-sed -i '' "s/Group nogroup/Group $group_culti/" /Applications/XAMPP/xamppfiles/etc/httpd.conf
-chown -R nobody /Applications/XAMPP/xamppfiles/var/mysql
-chmod -R 775 /Applications/XAMPP/xamppfiles/var/mysql
+chown -R $user_culti:$group_culti /Applications/cultibox
+sed -i '' "s/User nobody/User $user_culti/" /Applications/cultibox/xamppfiles/etc/httpd.conf
+sed -i '' "s/Group nogroup/Group $group_culti/" /Applications/cultibox/xamppfiles/etc/httpd.conf
+chown -R nobody /Applications/cultibox/xamppfiles/var/mysql
+chmod -R 775 /Applications/cultibox/xamppfiles/var/mysql
 
 
 /Applications/XAMPP/xamppfiles/xampp restart
--- 01_software/01_install/04_mac/pre-compil-linux.sh
+++ 01_software/01_install/04_mac/pre-compil-linux.sh
@@ -27,30 +27,31 @@ case "$1" in
       "snow-leopard" )
             rm -Rf ../01_src/01_xampp/*
             tar zxvfp xampp-mac-lite-1.7.3.tar.gz -C ../01_src/01_xampp/
-            cp -R ../../02_src/joomla ../01_src/01_xampp/XAMPP/xamppfiles/htdocs/cultibox
-            cat ../../CHANGELOG > ../01_src/01_xampp/XAMPP/VERSION.txt
+            mv ../01_src/01_xampp/XAMPP ../01_src/01_xampp/cultibox
+            cp -R ../../02_src/joomla ../01_src/01_xampp/cultibox/xamppfiles/htdocs/cultibox
+            cat ../../CHANGELOG > ../01_src/01_xampp/cultibox/VERSION.txt
 
-            cp conf-lampp/httpd.conf ../01_src/01_xampp/XAMPP/xamppfiles/etc/
-            cp conf-lampp/php.ini ../01_src/01_xampp/XAMPP/xamppfiles/etc/
-            cp conf-lampp/httpd-xampp.conf ../01_src/01_xampp/XAMPP/xamppfiles/etc/extra/
-            cp conf-lampp/my.cnf ../01_src/01_xampp/XAMPP/xamppfiles/etc/
-            cp -R ../../01_install/01_src/03_sd ../01_src/01_xampp/XAMPP/sd
-            cp -R ../../01_install/01_src/02_sql ../01_src/01_xampp/XAMPP/sql_install
-            mkdir ../01_src/01_xampp/XAMPP/package
-            cp conf-package/takecontrol.png ../01_src/01_xampp/XAMPP/package
-            cp conf-package/lgpl3.txt ../01_src/01_xampp/XAMPP/LICENSE.txt
-            cp conf-package/*.sh ../01_src/01_xampp/XAMPP/package/
-            cat ../../CHANGELOG > ../01_src/01_xampp/XAMPP/VERSION.txt
+            cp conf-lampp/httpd.conf ../01_src/01_xampp/cultibox/xamppfiles/etc/
+            cp conf-lampp/php.ini ../01_src/01_xampp/cultibox/xamppfiles/etc/
+            cp conf-lampp/httpd-xampp.conf ../01_src/01_xampp/cultibox/xamppfiles/etc/extra/
+            cp conf-lampp/my.cnf ../01_src/01_xampp/cultibox/xamppfiles/etc/
+            cp -R ../../01_install/01_src/03_sd ../01_src/01_xampp/cultibox/sd
+            cp -R ../../01_install/01_src/02_sql ../01_src/01_xampp/cultibox/sql_install
+            mkdir ../01_src/01_xampp/cultibox/package
+            cp conf-package/takecontrol.png ../01_src/01_xampp/cultibox/package
+            cp conf-package/lgpl3.txt ../01_src/01_xampp/cultibox/LICENSE.txt
+            cp conf-package/*.sh ../01_src/01_xampp/cultibox/package/
+            cat ../../CHANGELOG > ../01_src/01_xampp/cultibox/VERSION.txt
 
             #cp conf-script/* ../01_src/01_xampp/cultibox/opt/lampp/
-            cp ../../01_install/01_src/03_sd/firm.hex ../01_src/01_xampp/XAMPP/xamppfiles/htdocs/cultibox/tmp/
-            cp ../../01_install/01_src/03_sd/emetteur.hex ../01_src/01_xampp/XAMPP/xamppfiles/htdocs/cultibox/tmp/
-            cp ../../01_install/01_src/03_sd/sht.hex ../01_src/01_xampp/XAMPP/xamppfiles/htdocs/cultibox/tmp/
-            cp ../../01_install/01_src/03_sd/cultibox.ico ../01_src/01_xampp/XAMPP/xamppfiles/htdocs/cultibox/tmp/
-            cp ../../01_install/01_src/03_sd/cultibox.html ../01_src/01_xampp/XAMPP/xamppfiles/htdocs/cultibox/tmp/
+            cp ../../01_install/01_src/03_sd/firm.hex ../01_src/01_xampp/cultibox/xamppfiles/htdocs/cultibox/tmp/
+            cp ../../01_install/01_src/03_sd/emetteur.hex ../01_src/01_xampp/cultibox/xamppfiles/htdocs/cultibox/tmp/
+            cp ../../01_install/01_src/03_sd/sht.hex ../01_src/01_xampp/cultibox/xamppfiles/htdocs/cultibox/tmp/
+            cp ../../01_install/01_src/03_sd/cultibox.ico ../01_src/01_xampp/cultibox/xamppfiles/htdocs/cultibox/tmp/
+            cp ../../01_install/01_src/03_sd/cultibox.html ../01_src/01_xampp/cultibox/xamppfiles/htdocs/cultibox/tmp/
 
-            sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-noarch'/" ../01_src/01_xampp/XAMPP/sql_install/cultibox_fr.sql
-            sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-noarch'/" ../01_src/01_xampp/XAMPP/sql_install/cultibox_en.sql
+            sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-noarch'/" ../01_src/01_xampp/cultibox/sql_install/cultibox_fr.sql
+            sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-noarch'/" ../01_src/01_xampp/cultibox/sql_install/cultibox_en.sql
 
 
            #cp -R daemon ../01_src/01_xampp/cultibox/opt/lampp/
@@ -60,10 +61,11 @@ case "$1" in
            #fi
 
 
-            find ../01_src/01_xampp/XAMPP/ -name ".svn"|xargs rm -Rf
+            find ../01_src/01_xampp/cultibox/ -name ".svn"|xargs rm -Rf
             set +e
-            ssh root@$SERVER "if [ -d /Applications/XAMPP ]; then rm -Rf /Applications/XAMPP; fi"
-            rsync -av ../01_src/01_xampp/XAMPP root@$SERVER:/Applications/
+            ssh root@$SERVER "if [ -d /Applications/cultibox ]; then rm -Rf /Applications/cultibox; fi"
+            rsync -av ../01_src/01_xampp/cultibox root@$SERVER:/Applications/
+            ssh root@$SERVER "chmod -R root:wheel /Applications/cultibox"
             ssh root@$SERVER "if [ -d $WORK_DIR/cultibox.pmdoc ]; then rm -Rf $WORK_DIR/cultibox.pmdoc ; fi"
             rsync -av cultibox.pmdoc root@$SERVER:$WORK_DIR/
             ssh root@$SERVER "cd $WORK_DIR && /usr/bin/packagemaker --title cultibox -o cultibox-macosx_$VERSION.pkg --doc cultibox.pmdoc -v"
