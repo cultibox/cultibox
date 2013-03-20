f9f3756b86ca162b3d6baef5ab5ae9eacfaceab0
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-03-20 08:02:26 +0000

    pouet

---
+++ trunk/01_software/01_install/04_mac/conf-script/backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+user_culti=`who|head -1|awk -F" " '{print $1}'`
+group_culti=`who|head -1|awk -F" " '{print $1}'|xargs id -gn`
+
+if [ ! -d $HOME/.cultibox ]; then
+    mkdir $HOME/.cultibox
+    chown $user_culti:$group_culti $HOME/.cultibox
+fi
+/Applications/cultibox/xamppfiles/bin/mysqldump -u root -h localhost --port=3891 -pcultibox cultibox > $HOME/.cultibox/backup_cultibox
+chown $user_culti:$group_culti $HOME/.cultibox/backup_cultibox
