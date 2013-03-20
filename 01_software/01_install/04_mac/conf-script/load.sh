268283288abec056e5a9237be6b45ec159b98e6b
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-03-20 08:02:26 +0000

    pouet

---
+++ 01_software/01_install/04_mac/conf-script/load.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+user_culti=`who|head -1|awk -F" " '{print $1}'`
+group_culti=`who|head -1|awk -F" " '{print $1}'|xargs id -gn`
+
+if [ -f $HOME/.cultibox/backup_cultibox ]; then
+    /Applications/cultibox/xamppfiles/bin/mysql -u root -h localhost --port=3891 -pcultibox -e "DROP DATABASE cultibox;"
+    /Applications/cultibox/xamppfiles/bin/mysql -u root -h localhost --port=3891 -pcultibox -e "CREATE DATABASE cultibox;"
+    /Applications/cultibox/xamppfiles/bin/mysql -u root -h localhost --port=3891 -pcultibox cultibox < $HOME/.cultibox/backup_cultibox
+
+    if [ -f /Applications/cultibox/sql_install/update_sql.sql ]; then
+        /Applications/cultibox/xamppfiles/bin/mysql -u root -h localhost --port=3891 -pcultibox < /Applications/cultibox/sql_install/update_sql.sql 2>/dev/null
+    fi    
+fi
