a6991066ffa737375139213b71bcd27b58ad0af2
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-04-05 11:19:53 +0000

    Ajout des fichiers de création des bases pour it,es et de

--- 01_software/01_install/01_src/02_sql/cultibox_it.sql
+++ 01_software/01_install/01_src/02_sql/cultibox_it.sql
@@ -167,22 +167,22 @@ CREATE TABLE IF NOT EXISTS `plugs` (
 --
 
 INSERT INTO `plugs` (`id`, `PLUG_ID`, `PLUG_NAME`, `PLUG_TYPE`, `PLUG_TOLERANCE`, `PLUG_POWER`, `PLUG_POWER_MAX`, `PLUG_REGUL`, `PLUG_REGUL_SENSOR`, `PLUG_SENSO`, `PLUG_SENSS`, `PLUG_REGUL_VALUE`, `PLUG_ENABLED`) VALUES
-(1, '', 'Plug1', 'other', 1.0, NULL, '3500', 'False', '1', 'T', '+', 35.0,'True'),
-(2, '', 'Plug2', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(3, '', 'Plug3', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(4, '', 'Plug4', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(5, '', 'Plug5', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(6, '', 'Plug6', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(7, '', 'Plug7', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(8, '', 'Plug8', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(9, '', 'Plug9', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(10, '', 'Plug10', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(11, '', 'Plug11', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(12, '', 'Plug12', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(13, '', 'Plug13', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(14, '', 'Plug14', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(15, '', 'Plug15', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
-(16, '', 'Plug16', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True');
+(1, '', 'Spina1', 'other', 1.0, NULL, '3500', 'False', '1', 'T', '+', 35.0,'True'),
+(2, '', 'Spina2', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(3, '', 'Spina3', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(4, '', 'Spina4', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(5, '', 'Spina5', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(6, '', 'Spina6', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(7, '', 'Spina7', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(8, '', 'Spina8', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(9, '', 'Spina9', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(10, '', 'Spina10', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(11, '', 'Spina11', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(12, '', 'Spina12', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(13, '', 'Spina13', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(14, '', 'Spina14', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(15, '', 'Spina15', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True'),
+(16, '', 'Spina16', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True');
 
 -- --------------------------------------------------------
 
