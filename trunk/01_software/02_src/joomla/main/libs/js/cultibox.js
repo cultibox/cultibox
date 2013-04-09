eddfce1a72451848443e1b114bd01b49020803e4
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-04-09 07:51:11 +0000

    Correctifs pour le non rechargement de la page des programmes

--- trunk/01_software/02_src/joomla/main/libs/js/cultibox.js
+++ trunk/01_software/02_src/joomla/main/libs/js/cultibox.js
@@ -14,6 +14,7 @@ clean_highchart_message = function(message) { {
 
 $(document).ready(function() {
    // Affichage des tooltips sur les éléments avec un title
+   $("#selected_plug").prop("selectedIndex",0);
    $("[title]").tooltip({ position: { my: "left+15 center", at: "right center" } });
    $( ".pop_up_error" ).dialog({ width: 550, buttons: [ { text: "Ok", click: function() { $( this ).dialog( "close" ); } } ], hide: "fold", modal: true,  dialogClass: "popup_error" });
    $( ".pop_up_message" ).dialog({ width: 550, buttons: [ { text: "Ok", click: function() { $( this ).dialog( "close" ); } } ], hide: "fold", modal: true,  dialogClass: "popup_message"  });
