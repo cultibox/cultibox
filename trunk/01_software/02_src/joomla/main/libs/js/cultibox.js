91c8c1b79af99615f7575ed92345a223284d2e43
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-04-22 15:49:16 +0000

    Debug

--- trunk/01_software/02_src/joomla/main/libs/js/cultibox.js
+++ trunk/01_software/02_src/joomla/main/libs/js/cultibox.js
@@ -34,7 +34,6 @@ $(document).ready(function() {
         ico:    'http://localhost:6891/cultibox/main/libs/img/jquery.colourPicker.gif', 
         title:    false
    });
-   $("#selected_plug").prop("selectedIndex",0);
    $("[title]").tooltip({ position: { my: "left+15 center", at: "right center" } });
    $( ".pop_up_error" ).dialog({ width: 550, buttons: [ { text: "Ok", click: function() { $( this ).dialog( "close" ); } } ], hide: "fold", modal: true,  dialogClass: "popup_error" });
    $( ".pop_up_message" ).dialog({ width: 550, buttons: [ { text: "Ok", click: function() { $( this ).dialog( "close" ); } } ], hide: "fold", modal: true,  dialogClass: "popup_message"  });
