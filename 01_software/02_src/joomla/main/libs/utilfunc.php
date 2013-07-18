52ad3d5d2af151c868ad4d2c39f5c2caff49f946
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-07-18 10:46:37 +0000

    Remise en place de la modif du fichier utilfunc.php

--- 01_software/02_src/joomla/main/libs/utilfunc.php
+++ 01_software/02_src/joomla/main/libs/utilfunc.php
@@ -267,6 +267,7 @@ function get_power_value($file,&$array_line) {
 }
 //}}}
 
+
 // {{{ clean_log_file()
 // ROLE copy an empty file to clean a log file
 // IN $file             file to clean
@@ -1112,6 +1113,8 @@ function compare_sd_conf_file($sd_card="",$record_frequency,$update_frequency,$p
     $conf[]="ALARM_SENSO:000T";
     $conf[]="ALARM_SENSS:000+";
     $conf[]="RTC_OFFSET_:0000";
+    $conf[]="RESET_MINAX:0000";
+    $conf[]="PRESSION___:0000";
 
     $nb=0;
 
@@ -1185,6 +1188,8 @@ function write_sd_conf_file($sd_card,$record_frequency=1,$update_frequency=1,$po
       fputs($f,"ALARM_SENSO:$alarm_senso\r\n");
       fputs($f,"ALARM_SENSS:$alarm_senss\r\n");
       fputs($f,"RTC_OFFSET_:0000\r\n");
+      fputs($f,"RESET_MINAX:0000\r\n");
+      fputs($f,"PRESSION___:0000\r\n");
       fclose($f);
    } else {
       $out[]=__('ERROR_WRITE_SD_CONF');
