48ce1d2f05cfdcce6cbb0b81260e8f3b06c86579
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-10-04 17:20:57 +0000

    + Ajout d'une barre de progréssion pour la suppréssion des logs

--- 01_software/02_src/joomla/main/modules/external/delete_logs.php
+++ 01_software/02_src/joomla/main/modules/external/delete_logs.php
@@ -21,28 +21,14 @@ if((isset($_GET['type_reset']))&&(!empty($_GET['type_reset']))) {
 if((isset($type))&&(!empty($type))&&(isset($type_reset))&&(!empty($type_reset))) {
     if(strcmp($type_reset,"all")==0) {
         if(reset_log($type)) {
-                /*
-                $main_info[]=__('VALID_DELETE_LOGS');
-                $pop_up_message=popup_message(__('VALID_DELETE_LOGS'));
-                set_historic_value(__('VALID_DELETE_LOGS')." (".__('LOGS_PAGE').")","histo_info",$main_error);
-                //Reset power from the reset button
-if(!empty($reset_log_power)) {
-    if(reset_log("power",$main_error)) {
-        $main_info[]=__('VALID_DELETE_LOGS');
-        $pop_up_message=$pop_up_message.popup_message(__('VALID_DELETE_LOGS'));
-        set_historic_value(__('VALID_DELETE_LOGS')." (".__('LOGS_PAGE').")","histo_info",$main_error);
-    }
-}
-                */
                 echo "1";
         } else {
                 echo "-1";
         }
     } else {
-        if((isset($_GET['start']))&&(!empty($_GET['start']))&&(isset($_GET['end']))&&(!empty($_GET['end']))) {
+        if((isset($_GET['start']))&&(!empty($_GET['start']))) {
             $start=$_GET['start'];
-            $end=$_GET['end'];
-            if(reset_log($type,$start,$end)) {
+            if(reset_log($type,$start,$start)) {
                 echo "1";
             } else {
                 echo "-1";
