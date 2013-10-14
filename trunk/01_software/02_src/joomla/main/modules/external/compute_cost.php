366953705b1deab74a4ba669eef5dfb7aea09cac
yann.genevois <yann.genevois@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-10-14 13:29:56 +0000

    COmmit 2/3: modification de l'AJAX du calcul du cout, restera un dernier commit et pauffinage et c'est bon

--- trunk/01_software/02_src/joomla/main/modules/external/compute_cost.php
+++ trunk/01_software/02_src/joomla/main/modules/external/compute_cost.php
@@ -26,46 +26,17 @@ if((isset($startday))&&(!empty($startday))&&(isset($select_plug))&&(!empty($sele
     $power=0;
 
     //Computing cost value:
-    if(strcmp($select_plug,"distinct_all")!=0) {
+    if(strcmp("$type","theorical")==0) {
+        $power=get_theorical_power($select_plug,$cost_type,$main_error,$check);
+    } else {
+        $startTime = strtotime("$startday 12:00");
 
-        if(strcmp("$type","theorical")==0) {
-            $power=get_theorical_power($select_plug,$cost_type,$main_error,$check);
-        } else {
-            $startTime = strtotime("$startday 12:00");
-            $endTime = strtotime("$startday 12:00");
-
-            for ($i = $startTime; $i <= $endTime; $i = $i + 86400) {
-                $thisDate = date('Y-m-d', $i); // 2010-05-01, 2010-05-02, etc
-                $data_power=get_data_power($thisDate,$thisDate,$select_plug,$main_error);
-                $power=get_real_power($data_power,$cost_type,$main_error)+$real_power;
-                unset($data_power);
-            }
-        }
-
-        echo "$power";
-} else {
-    for($plugs=1;$plugs<=$nb_plugs;$plugs++) {
-       $theorical_power=get_theorical_power($plugs,$cost_type,$main_error,$check);
-
-       $startTime = strtotime("$startday 12:00");
-       $endTime = strtotime("$endday 12:00");
-       $real_power=0;
-
-       for ($i = $startTime; $i <= $endTime; $i = $i + 86400) {
-            $thisDate = date('Y-m-d', $i); // 2010-05-01, 2010-05-02, etc
-            $data_power=get_data_power($thisDate,$thisDate,$plugs,$main_error);
-            $real_power=get_real_power($data_power,$cost_type,$main_error)+$real_power;
-            unset($data_power);
-       }
-
-       $data_price[]= array(
-        "number" => $plugs,
-        "real" => "$real_power",
-        "theorical" => "$theorical_power",
-        "title" => "$title",
-        "color" => $GLOBALS['LIST_GRAPHIC_COLOR_PROGRAM'][$plugs-1]
-       );
+        $thisDate = date('Y-m-d', $startTime); // 2010-05-01, 2010-05-02, etc
+        $data_power=get_data_power($thisDate,$thisDate,$select_plug,$main_error);
+        $power=get_real_power($data_power,$cost_type,$main_error);
+        unset($data_power);
     }
-}
+
+    echo "$power";
 }
 
