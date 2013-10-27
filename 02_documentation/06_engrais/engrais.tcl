d6c4ae0dea5a3f8d28b99f479eed465ca4aff8cc
alliaume.rico@gmail.com <alliaume.rico@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-10-27 20:38:00 +0000

    MAJ du script de génération

--- 02_documentation/06_engrais/engrais.tcl
+++ 02_documentation/06_engrais/engrais.tcl
@@ -1,8 +1,11 @@
 set outPutDir [file join [file dirname [info script] ] out]
+set outPutDoc [file join [file dirname [info script] ] gui_soft_cal_engrais.wiki]
 
 set inputFile [file join [file dirname [info script] ] engrais.txt]
 
-proc initFile {fid marque substrat} {
+
+
+proc initFile {fid marque substrat programme} {
 	puts $fid {<?xml version="1.0" encoding="utf-8"?>}
 	puts $fid {<feed xmlns="http://www.w3.org/2005/Atom">}
 	puts $fid {   <updated>2013-04-16T13:14:57+02:00</updated>}
@@ -16,7 +19,7 @@ proc initFile {fid marque substrat} {
 	puts $fid {   </author>}
 	puts $fid {   <category term = "engrais" label = "engrais">}
 	puts $fid "    <substrat>${substrat}</substrat>"
-	puts $fid {    <periode>floraison</periode>}
+	puts $fid "    <periode>${programme}</periode>"
 	puts $fid "    <marque>${marque}</marque>"
 	puts $fid {   </category>}
 	puts $fid "   <link rel=\"self\" href=\"www.cultibox.fr/${marque}_${substrat}.xml\" />"
@@ -80,119 +83,124 @@ while {[eof $fidIn] != 1} {
 	set phase [lindex $UneLigne 3]
 	set remarque [lindex $UneLigne 4]
 	set ec [lindex $UneLigne 5]
+    set utilise [lindex $UneLigne 6]
 	
-	
-	if {$nomProgramme != $oldProgramme || $Substrat != $oldSubstrat} {
-		if {$fid != ""} {
-			closFile $fid
-			close $fid
-			set fid ""
-		}
-		if {$nomProgramme != ""} {
-			set fid [open [file join $outPutDir "[string map {" " "_"} ${marque}]_[string map {" " "_"} ${nomProgramme}]_[string map {" " "_"} $Substrat].xml"] w+]
-			initFile $fid $marque ${Substrat}
-		}
-	}
-	
-	if {$fid != ""} {
+    if {$utilise == 1} {
 
-		
-	
-		switch $phase {
-			"S1" -
-			"Graines" {
-				set start 0
-				set phase Croissance
-				set color green
-			}
-			"S2" -
-			"Plantules" {
-				set start 7
-				set phase Croissance
-				set color green
-			}
-			"S3" -
-			"Boutures" {
-				set start 14
-				set phase Croissance
-				set color green
-			}
-			"S4" -
-			"Veg 18 h" {
-				set start 21
-				set phase Floraison
-				set color orange
-			}
-			"S5" -
-			"Pass 12/12 " {
-				set start 28
-				set phase Floraison
-				set color orange
-			}
-			"S6" -
-			"Strech" {
-				set start 35
-				set phase Floraison
-				set color orange
-			}		
-			"S7" -
-			"1ers pistils" {
-				set start 42
-				set phase Floraison
-				set color orange
-			}		
-            "S8" -			
-			"Debut Flo" {
-				set start 49
-				set phase Floraison
-				set color orange
-			}
-			"S9" -
-			"Floraison" {
-				set start 56
-				set phase Floraison
-				set color orange
-			}
-			"S10" -
-			"Boom floral" {
-				set start 63
-				set phase Floraison
-				set color orange
-			}
-			"S11" -
-			"Fin floraison" {
-				set start 70
-				set phase Floraison
-				set color orange
-			}
-            "S12" -			
-			"Noir" {
-				set start 73
-				set phase Rincage
-				set color black
-			}			
-		}
+        if {$nomProgramme != $oldProgramme || $Substrat != $oldSubstrat} {
+            if {$fid != ""} {
+                closFile $fid
+                close $fid
+                set fid ""
+            }
+            if {$nomProgramme != ""} {
+                set fid [open [file join $outPutDir "[string map {" " "_"} ${marque}]_[string map {" " "_"} ${nomProgramme}]_[string map {" " "_"} $Substrat].xml"] w+]
+                initFile $fid $marque ${Substrat} $nomProgramme
+            }
+        }
+        
+        if {$fid != ""} {
 
-		startEntry $fid $start $phase $remarque $ec
-		
-		set idx 6
-		foreach Dosage [lrange $UneLigne 6 end] {
-			
-			if {$Dosage != 0} {
-				set unit "ml/l"
-				if {[lindex $EngraisName $idx] == "Mineral Magic"} {
-					set unit "g/l"
-				}
-				addNutriment $fid [lindex $EngraisName $idx] "${Dosage} ${unit}"
-			}
-			incr idx
-		}
-		closeEntry $fid $marque $color
-	}
+            
+        
+            switch $phase {
+                "S1" -
+                "Graines" {
+                    set start 0
+                    set phase Croissance
+                    set color green
+                }
+                "S2" -
+                "Plantules" {
+                    set start 7
+                    set phase Croissance
+                    set color green
+                }
+                "S3" -
+                "Boutures" {
+                    set start 14
+                    set phase Croissance
+                    set color green
+                }
+                "S4" -
+                "Veg 18 h" {
+                    set start 21
+                    set phase Floraison
+                    set color orange
+                }
+                "S5" -
+                "Pass 12/12 " {
+                    set start 28
+                    set phase Floraison
+                    set color orange
+                }
+                "S6" -
+                "Strech" {
+                    set start 35
+                    set phase Floraison
+                    set color orange
+                }		
+                "S7" -
+                "1ers pistils" {
+                    set start 42
+                    set phase Floraison
+                    set color orange
+                }		
+                "S8" -			
+                "Debut Flo" {
+                    set start 49
+                    set phase Floraison
+                    set color orange
+                }
+                "S9" -
+                "Floraison" {
+                    set start 56
+                    set phase Floraison
+                    set color orange
+                }
+                "S10" -
+                "Boom floral" {
+                    set start 63
+                    set phase Floraison
+                    set color orange
+                }
+                "S11" -
+                "Fin floraison" {
+                    set start 70
+                    set phase Floraison
+                    set color orange
+                }
+                "S12" -			
+                "Noir" {
+                    set start 77
+                    set phase Rincage
+                    set color black
+                }			
+            }
+
+            startEntry $fid $start $phase $remarque $ec
+            
+            set idx 7
+            foreach Dosage [lrange $UneLigne 7 end] {
+                
+                if {$Dosage != 0} {
+                    set unit "ml/l"
+                    if {[lindex $EngraisName $idx] == "Mineral Magic"} {
+                        set unit "g/l"
+                    }
+                    addNutriment $fid [lindex $EngraisName $idx] "${Dosage} ${unit}"
+                }
+                incr idx
+            }
+            closeEntry $fid $marque $color
+        }
+        
+        
+        set oldProgramme $nomProgramme
+        set oldSubstrat $Substrat
+    
+    }
 	
-	
-	set oldProgramme $nomProgramme
-	set oldSubstrat $Substrat
 	
 }
 
@@ -202,6 +210,147 @@ if {$fid != ""} {
 	set fid ""
 }
 
+close $fidIn
+
+
+
+# Génération de la doc
+set fid [open $outPutDoc w+]
+set fidIn [open $inputFile r]
+
+gets $fidIn UneLigne
+set EngraisMarque [split $UneLigne "\t"]
+
+gets $fidIn UneLigne
+set EngraisName [split $UneLigne "\t"]
+
+puts $fid {
+#summary Calendrier des engrais
+
+= Sommaire =
+<wiki:toc max_depth="3" />
+
+= Introduction =
+
+Le logiciel Cultibox vous permet de planifier les engrais que vous souhaitez appliquer.
+
+= Engrais =
+
+House And Garden
+
+Source : [http://www.house-garden.ca/ Schéma de culture]
+
+}
+
+set oldProgramme ""
+set oldSubstrat ""
+set oldmarque ""
+set semaine 1
+    
+while {[eof $fidIn] != 1} {
+	gets $fidIn UneLigne
+    set UneLigne [split $UneLigne "\t"]
+    set marque [lindex $UneLigne 0]
+    set nomProgramme [lindex $UneLigne 1]
+    set Substrat  [lindex $UneLigne 2]
+    set phase [lindex $UneLigne 3]
+    set remarque [lindex $UneLigne 4]
+    set ec [lindex $UneLigne 5]
+    set utilise [lindex $UneLigne 6]
+        
+    
+        
+    if {$utilise == 1 || [eof $fidIn]} {
+    
+        # Si ça change on écrit le vecteur
+        if {$Substrat != $oldSubstrat || $marque != $oldmarque || $nomProgramme != $oldProgramme } {
+            if {[array exists eng]} {
+                set change 0
+                
+                set listEngrais ""
+                set listSemaine ""
+                foreach name [array names eng] {
+                    if {[lindex [split $name ","] 1] != "remarque" && [lindex [split $name ","] 1] != "ec"} { 
+                    lappend listSemaine [lindex [split $name ","] 0]
+                    lappend listEngrais [lindex [split $name ","] 1]
+                    }
+                }
+                set listEngrais [lsort -unique $listEngrais]
+                set listSemaine [lsort -integer -unique $listSemaine]
+                
+                puts -nonewline $fid "|| *Semaine* || *Remarque* || *EC* "
+                foreach Engrais $listEngrais {
+                    puts -nonewline $fid "|| *${Engrais}* "
+                }
+                puts -nonewline $fid "||"
+                puts $fid ""
+                    
+                foreach Semaine $listSemaine {
+                    puts -nonewline $fid "|| Semaine $Semaine || $eng(${Semaine},remarque) || $eng(${Semaine},ec)"
+                    foreach Engrais $listEngrais {
+                        if {[array names eng ${Semaine},${Engrais}] != ""} {
+                            puts -nonewline $fid "|| $eng(${Semaine},${Engrais})"
+                        } else {
+                            puts -nonewline $fid "||  "
+                        }
+                    }
+                    puts -nonewline $fid "||"
+                    puts $fid ""
+                }
+                array unset eng
+                
+
+            }
+        }
+    
+        if {$marque != $oldmarque && $marque != ""} {
+            puts $fid ""
+            puts $fid "== ${marque} =="
+            puts $fid ""
+        }
+        
+        if {$nomProgramme != $oldProgramme && $nomProgramme != ""} {
+            puts $fid ""
+            puts $fid "=== ${nomProgramme} ==="
+            puts $fid ""
+        }
+        
+        if {$Substrat != $oldSubstrat && $Substrat != ""} {
+
+            set semaine 1
+
+            puts $fid ""
+            puts $fid "==== ${Substrat} ===="
+            puts $fid ""
+            
+        }
+        
+
+        set idx 7
+        set eng(${semaine},ec) $ec
+        set eng(${semaine},remarque) $remarque
+        foreach Dosage [lrange $UneLigne 7 end] {
+            
+            if {$Dosage != 0} {
+                set unit "ml/l"
+                if {[lindex $EngraisName $idx] == "Mineral Magic"} {
+                    set unit "g/l"
+                }
+                set eng(${semaine},[lindex $EngraisName $idx]) "${Dosage} ${unit}"
+            }
+            incr idx
+        }
+        incr semaine
+        
+                set oldProgramme $nomProgramme
+                set oldSubstrat $Substrat
+                set oldmarque $marque
+    }
+
+
+}
+
+close $fid
 close $fidIn
 
 
