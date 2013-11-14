68607b923042bc20fe5dc6d0be617c1df6a5d568
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-11-14 13:08:44 +0000

    Mose a jour du découpage de l'upgrade des scripts sql pour joomla

---
+++ trunk/01_software/01_install/01_src/02_sql/update_joomla-1.1.26.sql
@@ -0,0 +1,5 @@
+SET CHARACTER SET utf8;
+
+-- Version 1.1.26:
+UPDATE `cultibox_joomla`.`dkg45_menu` SET  published = "1" WHERE alias LIKE "wizard-%";
+
--- trunk/01_software/01_install/01_src/02_sql/update_sql-1.1.26.sql
+++ trunk/01_software/01_install/01_src/02_sql/update_sql-1.1.26.sql
@@ -2,5 +2,3 @@ SET CHARACTER SET utf8;
 
 -- Version 1.1.26:
 ALTER TABLE `cultibox`.`configuration` DROP COLUMN `SHOW_WIZARD`;
-UPDATE `cultibox_joomla`.`dkg45_menu` SET  published = "1" WHERE alias LIKE "wizard-%";
-
