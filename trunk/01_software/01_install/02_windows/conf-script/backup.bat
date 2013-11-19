2e298f66917bd6e08b341340203744433580cac3
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-11-19 13:58:09 +0000

    nouveau script de sauvegarde des données

--- trunk/01_software/01_install/02_windows/conf-script/backup.bat
+++ trunk/01_software/01_install/02_windows/conf-script/backup.bat
@@ -1,27 +1,58 @@
 @ECHO OFF
-mkdir backup
 
-xampp\mysql\bin\mysqldump.exe --no-create-db --no-create-info -u root -h 127.0.0.1 --port=3891 -pcultibox cultibox jqcalendar > ./backup/jqcalendar.sql
-If not errorlevel 0 del ./backup/jqcalendar.sql
+echo -----------------------------------------------------------------
+echo               Cultibox backup database script                    
+echo -----------------------------------------------------------------
+echo 
 
-xampp\mysql\bin\mysqldump.exe --no-create-db --no-create-info -u root -h 127.0.0.1 --port=3891 -pcultibox cultibox logs > ./backup/logs.sql
-If not errorlevel 0 del ./backup/logs.sql
+echo test > %HOMEPATH%\cultibox\test.txt
+if exist %HOMEPATH%\cultibox\test.txt goto EXIST
 
-xampp\mysql\bin\mysqldump.exe --no-create-db --no-create-info -u root -h 127.0.0.1 --port=3891 -pcultibox cultibox configuration > ./backup/configuration.sql
-If not errorlevel 0 del ./backup/configuration.sql
+mkdir %HOMEPATH%\cultibox
+echo   * %HOMEPATH%\cultibox already exists and will be used to store backup files
+goto START_BACKUP
 
-xampp\mysql\bin\mysqldump.exe --no-create-db --no-create-info -u root -h 127.0.0.1 --port=3891 -pcultibox cultibox plugs > ./backup/plugs.sql
-If not errorlevel 0 del ./backup/plugs.sql
+:EXIST
+del %HOMEPATH%\cultibox\test.txt >nul
 
-xampp\mysql\bin\mysqldump.exe --no-create-db --no-create-info -u root -h 127.0.0.1 --port=3891 -pcultibox cultibox power > ./backup/power.sql
-If not errorlevel 0 del ./backup/power.sql
+:START_BACKUP
+echo   * Exporting your current databae...
+C:\cultibox\xampp\mysql\bin\mysqldump.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891 cultibox > %HOMEPATH%\cultibox\backup_cultibox.bak.new
+If errorlevel 0 (
+    echo ... cultibox: OK
+) else (
+    del %HOMEPATH%\cultibox\backup_cultibox.bak.new
+    echo ==== Error during the backup of the cultibox database, exiting ====
+    echo ... NOK
+    pause
+    exit 1
+)
 
-xampp\mysql\bin\mysqldump.exe --no-create-db --no-create-info -u root -h 127.0.0.1 --port=3891 -pcultibox cultibox programs  > ./backup/programs.sql
-If not errorlevel 0 del ./backup/programs.sql
 
-xampp\mysql\bin\mysqldump.exe --no-create-db --no-create-info -u root -h 127.0.0.1 --port=3891 -pcultibox cultibox informations > ./backup/informations.sql
-If not errorlevel 0 del ./backup/informartions.sql
+C:\cultibox\xampp\mysql\bin\mysqldump.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891 cultibox_joomla > %HOMEPATH%\cultibox\backup_joomla.bak.new
+If errorlevel 0 (
+    echo ... cultibox: OK
+) else (
+    del %HOMEPATH%\cultibox\backup_cultibox.bak.new
+    echo ==== Error during the backup of the cultibox database, exiting ====
+    echo ... NOK
+    pause
+    exit 1
+)
 
-xampp\mysql\bin\mysqldump.exe --no-create-db --no-create-info -u root -h 127.0.0.1 --port=3891 -pcultibox cultibox historic > ./backup/historic.sql
-If not errorlevel 0 del ./backup/historic.sql
 
+echo   * Saving previous Cultibox backup database...
+If exist %HOMEPATH%\cultibox\backup_cultibox.bak (
+    move %HOMEPATH%\cultibox\backup_cultibox.bak %HOMEPATH%\cultibox\backup_cultibox.bak.old
+    echo ... OK
+)
+
+echo   * Saving previous Joomla backups database...
+If exist %HOMEPATH%\cultibox\backup_joomla.bak (
+    move %HOMEPATH%\cultibox\backup_joomla.bak %HOMEPATH%\cultibox\backup_joomla.bak.old
+    echo ... OK
+)
+
+move %HOMEPATH%\cultibox\backup_cultibox.bak.new %HOMEPATH%\cultibox\backup_cultibox.bak
+move %HOMEPATH%\cultibox\backup_joomla.bak.new %HOMEPATH%\cultibox\backup_joomla.bak
+exit 0
