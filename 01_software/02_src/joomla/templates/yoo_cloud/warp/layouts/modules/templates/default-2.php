591b12ce8acdac7715479e6b1760ad7c9bbf0a64
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-11-24 21:54:54 +0000

    Suppression du joomla

--- 01_software/02_src/joomla/templates/yoo_cloud/warp/layouts/modules/templates/default-2.php
+++
@@ -1,9 +0,0 @@
-<div class="module <?php echo $style; ?>">
-	<div class="deepest">
-
-		<?php echo $badge; ?>		
-		<?php if ($showtitle) echo $title; ?>
-		<?php echo $content; ?>
-
-	</div>
-</div>
