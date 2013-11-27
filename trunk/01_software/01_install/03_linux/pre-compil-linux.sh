ff86e5718221f907544defe3052ad2545ea5f691
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-11-27 15:56:56 +0000

    Ajout d'une recete all architecture pour linux pour les version >= 13.10

--- trunk/01_software/01_install/03_linux/pre-compil-linux.sh
+++ trunk/01_software/01_install/03_linux/pre-compil-linux.sh
@@ -9,8 +9,7 @@ DEST_DIR=../../01_install/01_src/01_xampp
 
 function usage {
             echo "usage: $0"
-            echo "                      ubuntu32|ubuntu64 <version> ?jenkins?"
-            echo "                      ubuntu32-admin|ubuntu64-admin <version> ?jenkins?"
+            echo "                      ubuntu32|ubuntu64|ubuntu-all <version> ?jenkins?"
             echo "                      clean"
             exit 1
 }
@@ -30,7 +29,7 @@ fi
 
 
 case "$1" in
-      "ubuntu64"|"ubuntu64-admin" )
+      "ubuntu64")
            (cd ../../../02_documentation/02_userdoc/ && tclsh ./parse_wiki.tcl && pdflatex documentation.tex && pdflatex documentation.tex)
            rm -Rf ../01_src/01_xampp/*
            mkdir ../01_src/01_xampp/cultibox
@@ -39,12 +38,7 @@ case "$1" in
            mkdir -p ../01_src/01_xampp/cultibox/usr/share/applications/
            cp ./conf-package/cultibox.desktop ../01_src/01_xampp/cultibox/usr/share/applications/
             
-
-           if [ "$1" == "ubuntu64" ]; then
-               tar zxvfp xampp-linux-1.8.3.tar.gz -C ../01_src/01_xampp/cultibox/opt/
-           else
-               tar zxvfp xampp-linux-admin-1.8.3.tar.gz -C ../01_src/01_xampp/cultibox/opt/
-           fi
+           tar zxvfp xampp-linux-1.8.3.tar.gz -C ../01_src/01_xampp/cultibox/opt/
 
            cp -R ../../02_src/joomla ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox
            cp ../../../02_documentation/02_userdoc/documentation.pdf ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox/main/docs/documentation_cultibox.pdf
@@ -78,10 +72,6 @@ EOF
             
            cp -R daemon ../01_src/01_xampp/cultibox/opt/lampp/
 
-           if [ "$1" == "ubuntu64-admin" ]; then
-                cp conf-lampp/config.inc.php ../01_src/01_xampp/cultibox/opt/lampp/phpmyadmin/
-           fi
-
            sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-amd64'/" ../01_src/01_xampp/cultibox/opt/lampp/sql_install/cultibox_fr.sql
            sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-amd64'/" ../01_src/01_xampp/cultibox/opt/lampp/sql_install/cultibox_en.sql
            sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-amd64'/" ../01_src/01_xampp/cultibox/opt/lampp/sql_install/cultibox_fr.sql
@@ -96,13 +86,9 @@ EOF
 
            cd ./../01_src/01_xampp/ && dpkg-deb --build cultibox
            
-           if [ "$1" == "ubuntu64" ]; then
-                mv cultibox.deb ../../03_linux/Output/cultibox-ubuntu-amd64_`echo $VERSION`.deb
-           else
-                mv cultibox.deb ../../03_linux/Output/cultibox-admin-ubuntu-amd64_`echo $VERSION`.deb
-           fi 
+           mv cultibox.deb ../../03_linux/Output/cultibox-ubuntu-amd64_`echo $VERSION`.deb
       ;;
-      "ubuntu32"|"ubuntu32-admin")
+      "ubuntu32")
             (cd ../../../02_documentation/02_userdoc/ && tclsh ./parse_wiki.tcl && tclsh ./parse_wiki.tcl && pdflatex documentation.tex)
             rm -Rf ../01_src/01_xampp/*
             mkdir ../01_src/01_xampp/cultibox
@@ -111,11 +97,7 @@ EOF
             mkdir -p ../01_src/01_xampp/cultibox/usr/share/applications/
             cp ./conf-package/cultibox.desktop ../01_src/01_xampp/cultibox/usr/share/applications/
 
-            if [ "$1" == "ubuntu32" ]; then
-                tar zxvfp xampp-linux-1.8.3.tar.gz -C ../01_src/01_xampp/cultibox/opt/
-            else
-                tar zxvfp xampp-linux-admin-1.8.3.tar.gz -C ../01_src/01_xampp/cultibox/opt/
-            fi
+            tar zxvfp xampp-linux-1.8.3.tar.gz -C ../01_src/01_xampp/cultibox/opt/
 
             cp -R ../../02_src/joomla ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox
             cp ../../../02_documentation/02_userdoc/documentation.pdf ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox/main/docs/documentation_cultibox.pdf
@@ -147,10 +129,6 @@ EOF
 
            cp -R daemon ../01_src/01_xampp/cultibox/opt/lampp/
 
-           if [ "$1" == "ubuntu32-admin" ]; then
-                cp conf-lampp/config.inc.php ../01_src/01_xampp/cultibox/opt/lampp/phpmyadmin/
-           fi
-
            #replacement of the old version number by the new one in VERSION file
            sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-i386'/" ../01_src/01_xampp/cultibox/opt/lampp/sql_install/cultibox_fr.sql
            sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-i386'/" ../01_src/01_xampp/cultibox/opt/lampp/sql_install/cultibox_en.sql
@@ -165,12 +143,65 @@ EOF
            mv ../01_src/01_xampp/cultibox/opt/lampp ../01_src/01_xampp/cultibox/opt/cultibox
            cd ./../01_src/01_xampp/ && dpkg-deb --build cultibox
 
-           if [ "$1" == "ubuntu32" ]; then
-                mv cultibox.deb ../../03_linux/Output/cultibox-ubuntu-i386_`echo $VERSION`.deb
-           else
-                mv cultibox.deb ../../03_linux/Output/cultibox-admin-ubuntu-i386_`echo $VERSION`.deb
-           fi
+           mv cultibox.deb ../../03_linux/Output/cultibox-ubuntu-i386_`echo $VERSION`.deb
       ;;
+        "ubuntu-all")
+            (cd ../../../02_documentation/02_userdoc/ && tclsh ./parse_wiki.tcl && tclsh ./parse_wiki.tcl && pdflatex documentation.tex)
+            rm -Rf ../01_src/01_xampp/*
+            mkdir ../01_src/01_xampp/cultibox
+            cp -R ./conf-package/DEBIAN-ALL ../01_src/01_xampp/cultibox/DEBIAN
+            mkdir ../01_src/01_xampp/cultibox/opt
+            mkdir -p ../01_src/01_xampp/cultibox/usr/share/applications/
+            cp ./conf-package/cultibox.desktop ../01_src/01_xampp/cultibox/usr/share/applications/
+
+            tar zxvfp xampp-linux-1.8.3.tar.gz -C ../01_src/01_xampp/cultibox/opt/
+
+            cp -R ../../02_src/joomla ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox
+            cp ../../../02_documentation/02_userdoc/documentation.pdf ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox/main/docs/documentation_cultibox.pdf
+            cat ../../CHANGELOG > ../01_src/01_xampp/cultibox/opt/lampp/VERSION.txt
+
+           cp conf-lampp/httpd.conf ../01_src/01_xampp/cultibox/opt/lampp/etc/
+           cp conf-lampp/php.ini ../01_src/01_xampp/cultibox/opt/lampp/etc/
+           cp conf-lampp/httpd-xampp.conf ../01_src/01_xampp/cultibox/opt/lampp/etc/extra/
+           cp conf-lampp/properties.ini ../01_src/01_xampp/cultibox/opt/lampp/
+
+           cp -R conf-script ../01_src/01_xampp/cultibox/opt/lampp/run
+           cp conf-lampp/my.cnf ../01_src/01_xampp/cultibox/opt/lampp/etc/
+           cp conf-package/cultibox.png ../01_src/01_xampp/cultibox/opt/lampp/
+           cp conf-package/lgpl3.txt ../01_src/01_xampp/cultibox/opt/lampp/LICENSE.txt
+            cat > ../01_src/01_xampp/cultibox/opt/lampp/etc/my-extra.cnf << "EOF" 
+[client]
+user="root"
+password="cultibox"
+EOF
+           cp -R ../../01_install/01_src/03_sd ../01_src/01_xampp/cultibox/opt/lampp/sd
+           cp -R ../../01_install/01_src/02_sql ../01_src/01_xampp/cultibox/opt/lampp/sql_install
+           sed -i "s/\`VERSION\` = '.*/\`VERSION\` = '`echo $VERSION`-noarch' WHERE \`configuration\`.\`id\` =1;/" ../01_src/01_xampp/cultibox/opt/lampp/sql_install/update_sql.sql
+           cp ../../01_install/01_src/03_sd/firm.hex ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox/tmp/
+           cp -R ../../01_install/01_src/03_sd/bin ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox/tmp/
+           cp ../../01_install/01_src/03_sd/cultibox.ico ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox/tmp/
+           cp ../../01_install/01_src/03_sd/cultibox.html ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox/tmp/
+           cp -R ../../01_install/01_src/03_sd/cnf ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox/tmp/
+           cp -R ../../01_install/01_src/03_sd/logs ../01_src/01_xampp/cultibox/opt/lampp/htdocs/cultibox/tmp/
+
+           cp -R daemon ../01_src/01_xampp/cultibox/opt/lampp/
+
+           #replacement of the old version number by the new one in VERSION file
+           sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-noarch'/" ../01_src/01_xampp/cultibox/opt/lampp/sql_install/cultibox_fr.sql
+           sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-noarch'/" ../01_src/01_xampp/cultibox/opt/lampp/sql_install/cultibox_en.sql
+           sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-noarch'/" ../01_src/01_xampp/cultibox/opt/lampp/sql_install/cultibox_de.sql
+           sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-noarch'/" ../01_src/01_xampp/cultibox/opt/lampp/sql_install/cultibox_it.sql
+           sed -i "s/'[0-9]\+\.[0-9]\+\.[0-9]\+'/'`echo $VERSION`-noarch'/" ../01_src/01_xampp/cultibox/opt/lampp/sql_install/cultibox_es.sql
+
+           sed -i "s/Version: .*/Version: `echo $VERSION`-ubuntu/g" ../01_src/01_xampp/cultibox/DEBIAN/control
+           sed -i "s/Version=.*/Version=`echo $VERSION`/g" ../01_src/01_xampp/cultibox/usr/share/applications/cultibox.desktop
+
+           find ./../01_src/01_xampp/cultibox/opt/lampp -name ".svn"|xargs rm -Rf
+           mv ../01_src/01_xampp/cultibox/opt/lampp ../01_src/01_xampp/cultibox/opt/cultibox
+           cd ./../01_src/01_xampp/ && dpkg-deb --build cultibox
+
+           mv cultibox.deb ../../03_linux/Output/cultibox-ubuntu-noarch_`echo $VERSION`.deb
+        ;;
       "clean")
             rm -Rf ../01_src/01_xampp/*
       ;;
