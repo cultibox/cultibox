8f40f28e58fa2739965d65299527090594fb9ed2
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-12-01 21:42:54 +0000

    Ajout des fichiers de changement d'heure comme fichier XML

--- trunk/01_software/01_install/02_windows/conf-script/load.bat
+++ trunk/01_software/01_install/02_windows/conf-script/load.bat
@@ -17,12 +17,11 @@ if NOT %ERRORLEVEL% EQU 0  (
 )
 
 
-echo * Loading calendar database...
-If exist "%userprofile%\cultibox\calendar.sql" (
-    for %%R in ("%userprofile%\cultibox\calendar.sql") do (
-        if not %%~zR equ 0 (
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox -e "DELETE FROM calendar"
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox < "%userprofile%\cultibox\calendar.sql"
+echo * Loading Cultibox database...
+If exist "%userprofile%\cultibox\backup_cultibox.sql" (
+        c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  -e "DROP DATABASE CULTIBOX;"
+        c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  -e "CREATE DATABASE cultibox;"
+        c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox < "%userprofile%\cultibox\backup_cultibox.sql"
             if %ERRORLEVEL% EQU 0  (
                 echo ... OK
             ) else (
@@ -35,155 +34,5 @@ If exist "%userprofile%\cultibox\calendar.sql" (
     )
 )
 
-
-echo * Loading logs database...
-If exist "%userprofile%\cultibox\logs.sql" (
-    for %%R in ("%userprofile%\cultibox\logs.sql") do (
-        if not %%~zR equ 0 (
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox -e "DELETE FROM logs"
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox < "%userprofile%\cultibox\logs.sql"
-            if %ERRORLEVEL% EQU 0  (
-                echo ... OK
-            ) else (
-                echo ===== Error loading database, exiting... ====
-                echo ... NOK
-                pause
-                exit 1
-            )
-        )
-    )
-)
-
-
-echo * Loading configuration database...
-If exist "%userprofile%\cultibox\configuration.sql" (
-    for %%R in ("%userprofile%\cultibox\configuration.sql") do (
-        if not %%~zR equ 0 (
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox -e "DELETE FROM configuration"
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox < "%userprofile%\cultibox\configuration.sql"
-            if %ERRORLEVEL% EQU 0  (
-                echo ... OK
-            ) else (
-                echo ===== Error loading database, exiting... ====
-                echo ... NOK
-                pause
-                exit 1
-            )
-        )
-    )
-)
-
-
-echo * Loading plugs database...
-If exist "%userprofile%\cultibox\plugs.sql" (
-    for %%R in ("%userprofile%\cultibox\plugs.sql") do (
-        if not %%~zR equ 0 (
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox -e "DELETE FROM plugs"
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox < "%userprofile%\cultibox\plugs.sql"
-            if %ERRORLEVEL% EQU 0  (
-                echo ... OK
-            ) else (
-                echo ===== Error loading database, exiting... ====
-                echo ... NOK
-                pause
-                exit 1
-            )
-        )
-    )
-)
-
-
-echo * Loading power database...
-If exist "%userprofile%\cultibox\power.sql" (
-    for %%R in ("%userprofile%\cultibox\power.sql") do (
-        if not %%~zR equ 0 (
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox -e "DELETE FROM power"
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox < "%userprofile%\cultibox\power.sql"
-            if %ERRORLEVEL% EQU 0  (
-                echo ... OK
-            ) else (
-                echo ===== Error loading database, exiting... ====
-                echo ... NOK
-                pause
-                exit 1
-            )
-        )
-    )
-)
-
-
-echo * Loading programs database...
-If exist "%userprofile%\cultibox\programs.sql" (
-    for %%R in ("%userprofile%\cultibox\programs.sql") do (
-        if not %%~zR equ 0 (
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox -e "DELETE FROM programs"
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox < "%userprofile%\cultibox\programs.sql"
-            if %ERRORLEVEL% EQU 0  (
-                echo ... OK
-            ) else (
-                echo ===== Error loading database, exiting... ====
-                echo ... NOK
-                pause
-                exit 1
-            )
-        )
-    )
-)
-
-
-echo * Loading informations database...
-If exist "%userprofile%\cultibox\informations.sql" (
-    for %%R in ("%userprofile%\cultibox\informations.sql") do (
-        if not %%~zR equ 0 (
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox -e "DELETE FROM informations"
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox < "%userprofile%\cultibox\informations.sql"
-            if %ERRORLEVEL% EQU 0  (
-                echo ... OK
-            ) else (
-                echo ===== Error loading database, exiting... ====
-                echo ... NOK
-                pause
-                exit 1
-            )
-        )
-    )
-)
-
-
-echo * Loading historic database...
-If exist "%userprofile%\cultibox\historic.sql" (
-    for %%R in ("%userprofile%\cultibox\historic.sql") do (
-        if not %%~zR equ 0 (
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox -e "DELETE FROM historic"
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox < "%userprofile%\cultibox\historic.sql"
-            if %ERRORLEVEL% EQU 0  (
-                echo ... OK
-            ) else (
-                echo ===== Error loading database, exiting... ====
-                echo ... NOK
-                pause
-                exit 1
-            )
-        )
-    )
-)
-
-
-echo * Loading notes database...
-If exist "%userprofile%\cultibox\notes.sql" (
-    for %%R in ("%userprofile%\cultibox\notes.sql") do (
-        if not %%~zR equ 0 (
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox -e "DELETE FROM notes"
-            c:\cultibox\xampp\mysql\bin\mysql.exe --defaults-extra-file="C:\cultibox\xampp\mysql\bin\my-extra.cnf" -h 127.0.0.1 --port=3891  cultibox < "%userprofile%\cultibox\notes.sql"
-            if %ERRORLEVEL% EQU 0  (
-                echo ... OK
-            ) else (
-                echo ===== Error loading database, exiting... ====
-                echo ... NOK
-                pause
-                exit 1
-            )
-        )
-    )
-)
-
+pause
+exit 0
