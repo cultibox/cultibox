ca605c7078ce10f5776e5193e6b74bd5c6a6db1f
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2013-12-09 12:01:04 +0000

    Ajout script load/backup for linux

--- trunk/01_software/01_install/03_linux/conf-script/backup.sh
+++ trunk/01_software/01_install/03_linux/conf-script/backup.sh
@@ -18,39 +18,22 @@ else
     echo "  * $home/.cultibox already exists and will be used to store backup files"
 fi
 
+echo "  * Saving previous Cultibox backup database..."
+if [ -f $home/.cultibox/backup_cultibox.sql ]; then
+    mv $home/.cultibox/backup_cultibox.sql $home/.cultibox/backup_cultibox.sql.old
+    echo "... OK"
+fi
+
+
 echo "  * Exporting your current databae..."
-/opt/cultibox/bin/mysqldump --defaults-extra-file=/opt/cultibox/etc/my-extra.cnf -h 127.0.0.1 --port=3891 cultibox > $home/.cultibox/backup_cultibox.bak.new
+/opt/cultibox/bin/mysqldump --defaults-extra-file=/opt/cultibox/etc/my-extra.cnf -h 127.0.0.1 --port=3891 cultibox > $home/.cultibox/backup_cultibox.sql
 if [ $? -eq 0 ]; then
     echo "... cultibox: OK"
 else 
-    rm $home/.cultibox/backup_cultibox.bak.new
     echo "==== Error during the backup of the cultibox database, exiting ===="
     echo "... NOK"
     exit 1
 fi
 
-/opt/cultibox/bin/mysqldump --defaults-extra-file=/opt/cultibox/etc/my-extra.cnf -h 127.0.0.1 --port=3891 cultibox_joomla > $home/.cultibox/backup_joomla.bak.new
-if [ $? -eq 0 ]; then
-    echo "... joomla: OK"
-else
-    rm $home/.cultibox/backup_joomla.bak.new
-    echo "==== Error during the backup of the joomla database, exiting ===="
-    echo "... NOK"
-    exit 1
-fi
-
-#Saving the database in the directory $home/.cultibox:
-echo "  * Saving previous Cultibox backup database..."
-if [ -f $home/.cultibox/backup_cultibox.bak ]; then
-    mv $home/.cultibox/backup_cultibox.bak $home/.cultibox/backup_cultibox.bak.old
-    echo "... OK"
-fi
-echo "  * Saving previous Joomla backups database..."
-if [ -f $home/.cultibox/backup_joomla.bak ]; then
-    mv $home/.cultibox/backup_joomla.bak $home/.cultibox/backup_joomla.bak.old
-    echo "... OK"
-fi
-mv $home/.cultibox/backup_cultibox.bak.new $home/.cultibox/backup_cultibox.bak
-mv $home/.cultibox/backup_joomla.bak.new $home/.cultibox/backup_joomla.bak
 chown $user_culti:$group_culti $home/.cultibox/*
 exit 0
