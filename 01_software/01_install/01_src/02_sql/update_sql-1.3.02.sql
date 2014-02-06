d44a1bc020b6a8e72ab02f56a86bc73d3534d422
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-02-06 11:12:51 +0000

    correction update sql

--- 01_software/01_install/01_src/02_sql/update_sql-1.3.02.sql
+++ 01_software/01_install/01_src/02_sql/update_sql-1.3.02.sql
@@ -6,4 +6,3 @@ ALTER TABLE `cultibox`.`configuration` ADD `WIFI_SSID` VARCHAR(32);
 ALTER TABLE `cultibox`.`configuration` ADD `WIFI_KEY_TYPE` VARCHAR(10) NOT NULL DEFAULT 'NONE';
 ALTER TABLE `cultibox`.`configuration` ADD `WIFI_PASSWORD` VARCHAR(63);
 
-
