ecd62ad03078b82d665aa30ffb83545e73a426dc
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-04-26 08:48:32 +0000

    + Correction de l'affichage de l'oeil

--- 01_software/02_src/joomla/main/modules/external/get_variable.php
+++ 01_software/02_src/joomla/main/modules/external/get_variable.php
@@ -1,42 +1,47 @@
 <?php 
+//Affiche la valeur d'une variable PHP, script appelé par Ajax par le fichier cultibox.js pour récupérer des informations
+// stockées côté serveur:
 
 require_once('../../libs/utilfunc.php');
 
-
+//On démarre une SESSION si ce n'est pas déja le cas:
 if (!isset($_SESSION)) {
     session_start();
 }
 
-
+//Récupération du nom de la variable, par convention interne, les noms de variable de SESSION sont 
+//toujours en majuscule, on capitalise donc le nom récupéré:
 if((isset($_GET['name']))&&(!empty($_GET['name']))) {
-    $name=$_GET['name'];
+    $name=strtoupper($_GET['name']);
 }
 
 
 if((!isset($name))||(empty($name))) {
-    return 0;
-}
-
-
-switch ($name) {
-    case 'load_log':
-        if((isset($_SESSION['LOAD_LOG']))&&(!empty($_SESSION['LOAD_LOG']))) {
-            echo $_SESSION['LOAD_LOG'];
-        }
-        break;
-    case 'sd_card':
-        echo get_sd_card();
-        break;
-    case 'important_calendar':
-        if((isset($_SESSION['IMPORTANT']))&&(!empty($_SESSION['IMPORTANT']))) {
-            echo $_SESSION['IMPORTANT'];
-        }
-        break;
-    case 'tooltip_msg_box':
-        if((isset($_SESSION['TOOLTIP_MSG_BOX']))&&(!empty($_SESSION['TOOLTIP_MSG_BOX']))) {
-            echo $_SESSION['TOOLTIP_MSG_BOX'];
-        }
-        break;
+    //On affiche 0 si la fonction est appelée sans le nom de la variable:
+    echo json_encode("0");
+} else {
+    switch($name) {
+        case 'LOAD_LOG':
+            if((isset($_SESSION['LOAD_LOG']))&&(!empty($_SESSION['LOAD_LOG']))) {
+                echo json_encode($_SESSION['LOAD_LOG']);
+            }
+            break;
+        case 'SD_CARD':
+            echo get_sd_card();
+            break;
+        case 'IMPORTANT':
+            if((isset($_SESSION['IMPORTANT']))&&(!empty($_SESSION['IMPORTANT']))) {
+                echo json_encode($_SESSION['IMPORTANT']);
+            }
+            break;
+        case 'TOOLTIP_MSG_BOX':
+            if((isset($_SESSION['TOOLTIP_MSG_BOX']))&&(!empty($_SESSION['TOOLTIP_MSG_BOX']))) {
+                echo json_encode($_SESSION['TOOLTIP_MSG_BOX']);
+            }
+            break;
+        default:
+            echo json_encode("0");
+    }
 }
 
 ?>
