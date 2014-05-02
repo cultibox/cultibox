d6f10cfb0ee4d12e864ec192ec9bfead0cbb4c04
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-05-02 09:00:39 +0000

    + Modification du chargement des logs en fonction des capteurs afin d'intégrer les nouveaux capteurs dans la Cultibox

--- 01_software/01_install/01_src/02_sql/update_sql-1.3.06.sql
+++ 01_software/01_install/01_src/02_sql/update_sql-1.3.06.sql
@@ -4,4 +4,6 @@ SET CHARACTER SET utf8;
 INSERT INTO `cultibox`.`sensors` (`id`, `type`) VALUES (5, 0),(6, 0);
 ALTER TABLE `cultibox`.`sensors` CHANGE `type`  `type` varchar(1) NOT NULL DEFAULT '0';
 ALTER TABLE `cultibox`.`configuration` ADD  `RTC_OFFSET` DECIMAL(3,2) NOT NULL DEFAULT '0';
-
+ALTER TABLE `cultibox`.`logs` CHANGE `temperature` `record1` int(4) DEFAULT NULL;
+ALTER TABLE `cultibox`.`logs` CHANGE `humidity` `record2` int(4) DEFAULT NULL;
+ALTER TABLE `cultibox`.`logs` DROP `type_sensor`;
