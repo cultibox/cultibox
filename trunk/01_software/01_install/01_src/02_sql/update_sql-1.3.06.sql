f8495014b50cd51e98aeb76ebc8df9a4ceaadaff
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-05-06 12:16:59 +0000

    Correction de l'intégration et du remplissage de la table sensors en fonction des logs présents

--- trunk/01_software/01_install/01_src/02_sql/update_sql-1.3.06.sql
+++ trunk/01_software/01_install/01_src/02_sql/update_sql-1.3.06.sql
@@ -3,6 +3,11 @@ SET CHARACTER SET utf8;
 -- Version 1.3.04:
 INSERT INTO `cultibox`.`sensors` (`id`, `type`) VALUES (5, 0),(6, 0);
 ALTER TABLE `cultibox`.`sensors` CHANGE `type`  `type` varchar(1) NOT NULL DEFAULT '0';
+UPDATE `cultibox`.`sensors` set `type` = (SELECT `type_sensor` FROM `cultibox`.`logs` where `sensor_nb`=1 GROUP BY `type_sensor`) WHERE id=1;
+UPDATE `cultibox`.`sensors` set `type` = (SELECT `type_sensor` FROM `cultibox`.`logs` where `sensor_nb`=2 GROUP BY `type_sensor`) WHERE id=2;
+UPDATE `cultibox`.`sensors` set `type` = (SELECT `type_sensor` FROM `cultibox`.`logs` where `sensor_nb`=3 GROUP BY `type_sensor`) WHERE id=3;
+UPDATE `cultibox`.`sensors` set `type` = (SELECT `type_sensor` FROM `cultibox`.`logs` where `sensor_nb`=4 GROUP BY `type_sensor`) WHERE id=4;
+UPDATE `cultibox`.`sensors` set `type` = '0' WHERE `type` LIKE '';
 ALTER TABLE `cultibox`.`configuration` ADD  `RTC_OFFSET` DECIMAL(3,2) NOT NULL DEFAULT '0';
 ALTER TABLE `cultibox`.`logs` CHANGE `temperature` `record1` int(4) DEFAULT NULL;
 ALTER TABLE `cultibox`.`logs` CHANGE `humidity` `record2` int(4) DEFAULT NULL;
