1525677fc18cdb6845f03a6d5ac78d56a0e213ff
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-05-16 10:28:55 +0000

    Mise à jour des requettes SQL pour la suppression de l'onglet historique

--- 01_software/01_install/01_src/02_sql/update_joomla-1.3.07.sql
+++ 01_software/01_install/01_src/02_sql/update_joomla-1.3.07.sql
@@ -1,9 +1,8 @@
 SET CHARACTER SET utf8;
 
 -- Version 1.3.07
-DELETE FROM `cultibox_joomla`.`dkg45_content` WHERE `id`=3;
-DELETE FROM `cultibox_joomla`.`dkg45_content` WHERE `id`=163;
-DELETE FROM `cultibox_joomla`.`dkg45_content` WHERE `id`=171;
-DELETE FROM `cultibox_joomla`.`dkg45_content` WHERE `id`=183;
-DELETE FROM `cultibox_joomla`.`dkg45_content` WHERE `id`=193;
-DELETE FROM `cultibox_joomla`.`dkg45_content` WHERE `id`=202;
+DELETE FROM `cultibox_joomla`.`dkg45_content` WHERE `title` LIKE "historic%";
+DELETE FROM `cultibox_joomla`.`dkg45_assets` WHERE `title` LIKE "historic";
+DELETE FROM `cultibox_joomla`.`dkg45_menu` WHERE `alias` LIKE "historic-%";
+DELETE FROM `cultibox_joomla`.`dkg45_redirect_links` WHERE `referer` LIKE "http://localhost:6891/cultibox/index.php/historic%";
+DELETE FROM `cultibox_joomla`.`dkg45_redirect_links` WHERE `old_url` LIKE "http://localhost:6891/cultibox/index.php/historic%";
