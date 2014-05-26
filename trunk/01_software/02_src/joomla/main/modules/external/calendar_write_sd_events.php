e1042e1b2b8078d378b1eabc332478f8c8920bc5
alliaume.rico@gmail.com <alliaume.rico@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-05-26 18:12:54 +0000

    Ajout de la création automatique des programmes dans la page alendrier

--- trunk/01_software/02_src/joomla/main/modules/external/calendar_write_sd_events.php
+++ trunk/01_software/02_src/joomla/main/modules/external/calendar_write_sd_events.php
@@ -9,6 +9,23 @@ require_once('../../libs/utilfunc_sd_card.php');
 // Retrieve start and end param (Unix time format (s))
 $sd_card = $_GET['sd_card'];
 
+// Create all plugXX programm
+// Read program index
+$program_index = array();
+program\get_program_index_info($program_index);
+
+// Foreach programm, create the programm
+foreach ($program_index as $key => $value)
+{
+    // Read from database program
+    $program = create_program_from_database($main_error,$value['program_idx']);
+    
+    // SAve programm on SD
+    save_program_on_sd($sd_card,$program,"plu" . $value['plugv_filename']);
+    
+}
+
+
 $data = array();
 calendar\read_event_from_db($data);
 
