dc792d838b0357d7534b4f1956143c163e9abb6f
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-07-18 16:32:13 +0000

    changelog

--- 01_software/01_install/01_src/02_sql/cultibox_es.sql
+++ 01_software/01_install/01_src/02_sql/cultibox_es.sql
@@ -153,7 +153,6 @@ CREATE TABLE IF NOT EXISTS `plugs` (
   `PLUG_SENSO` varchar(1) NOT NULL DEFAULT 'T',
   `PLUG_SENSS` varchar(1) NOT NULL DEFAULT '+',
   `PLUG_REGUL_VALUE` decimal(3,1) NOT NULL DEFAULT '35.0',
-  `PLUG_ENABLED` varchar(5) NOT NULL DEFAULT 'True',
   `PLUG_SECOND_TOLERANCE` DECIMAL( 3, 1 ) NOT NULL DEFAULT '0.0',
   `PLUG_COMPUTE_METHOD` VARCHAR( 1 ) NOT NULL DEFAULT 'M',
   PRIMARY KEY (`id`)
@@ -163,23 +162,23 @@ CREATE TABLE IF NOT EXISTS `plugs` (
 -- Dumping data for table `plugs`
 --
 
-INSERT INTO `plugs` (`id`, `PLUG_ID`, `PLUG_NAME`, `PLUG_TYPE`, `PLUG_TOLERANCE`, `PLUG_POWER`, `PLUG_POWER_MAX`, `PLUG_REGUL`, `PLUG_REGUL_SENSOR`, `PLUG_SENSO`, `PLUG_SENSS`, `PLUG_REGUL_VALUE`, `PLUG_ENABLED`,`PLUG_SECOND_TOLERANCE`,`PLUG_COMPUTE_METHOD`) VALUES
-(1, '', 'Enchufe1', 'other', 1.0, NULL, '3500', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(2, '', 'Enchufe2', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(3, '', 'Enchufe3', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(4, '', 'Enchufe4', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(5, '', 'Enchufe5', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(6, '', 'Enchufe6', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(7, '', 'Enchufe7', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(8, '', 'Enchufe8', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(9, '', 'Enchufe9', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(10, '', 'Enchufe10', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(11, '', 'Enchufe11', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(12, '', 'Enchufe12', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(13, '', 'Enchufe13', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(14, '', 'Enchufe14', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(15, '', 'Enchufe15', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M'),
-(16, '', 'Enchufe16', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,'True',0.0,'M');
+INSERT INTO `plugs` (`id`, `PLUG_ID`, `PLUG_NAME`, `PLUG_TYPE`, `PLUG_TOLERANCE`, `PLUG_POWER`, `PLUG_POWER_MAX`, `PLUG_REGUL`, `PLUG_REGUL_SENSOR`, `PLUG_SENSO`, `PLUG_SENSS`, `PLUG_REGUL_VALUE`, `PLUG_SECOND_TOLERANCE`,`PLUG_COMPUTE_METHOD`) VALUES
+(1, '', 'Enchufe1', 'other', 1.0, NULL, '3500', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(2, '', 'Enchufe2', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(3, '', 'Enchufe3', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(4, '', 'Enchufe4', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(5, '', 'Enchufe5', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(6, '', 'Enchufe6', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(7, '', 'Enchufe7', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(8, '', 'Enchufe8', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(9, '', 'Enchufe9', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(10, '', 'Enchufe10', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(11, '', 'Enchufe11', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(12, '', 'Enchufe12', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(13, '', 'Enchufe13', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(14, '', 'Enchufe14', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(15, '', 'Enchufe15', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M'),
+(16, '', 'Enchufe16', 'other', 1.0, NULL, '1000', 'False', '1', 'T', '+', 35.0,0.0,'M');
 
 -- --------------------------------------------------------
 
---
+++ 01_software/01_install/01_src/02_sql/update_sql-1.4.03.sql
@@ -0,0 +1,5 @@
+SET CHARACTER SET utf8;
+
+-- Version 1.4.03:
+ALTER TABLE `cultibox`.`plugs` DROP COLUMN `PLUG_ENABLED`;
+
