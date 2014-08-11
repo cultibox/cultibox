7047513996a25c49302e13183001353769877da4
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-08-11 17:41:38 +0000

    Correction de la vérification des updates

--- trunk/01_software/02_src/joomla/main/scripts/post_script.php
+++ trunk/01_software/02_src/joomla/main/scripts/post_script.php
@@ -2,12 +2,8 @@
     // IN DEV : Add check sd card message
     include('main/libs/js/check_sd.js'); 
     
-    // Check software version if needed
-    if(get_configuration("CHECK_UPDATE",$main_error) == "True")
-    {
-        $main_info['check_version_progress'] = __('INFO_UPDATE_CHECKING') . "<img src='../../main/libs/img/waiting_small.gif' />";
-        include('main/libs/js/check_version.js'); 
-    }
+    $main_info['check_version_progress'] = __('INFO_UPDATE_CHECKING') . "<img src='../../main/libs/img/waiting_small.gif' />";
+    include('main/libs/js/check_version.js'); 
     
     include('main/libs/js/send_info_error.js');
     
