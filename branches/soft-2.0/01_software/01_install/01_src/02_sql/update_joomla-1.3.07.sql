a81bb7c60d083b7c8e842f57148f7034aa3d53e9
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-09-13 12:38:23 +0000

    Branch 2.0: clean de joomla

--- branches/soft-2.0/01_software/01_install/01_src/02_sql/update_joomla-1.3.07.sql
+++
@@ -1,8 +0,0 @@
-SET CHARACTER SET utf8;
-
--- Version 1.3.07
-DELETE FROM `cultibox_joomla`.`dkg45_content` WHERE `title` LIKE "historic%";
-DELETE FROM `cultibox_joomla`.`dkg45_assets` WHERE `title` LIKE "historic";
-DELETE FROM `cultibox_joomla`.`dkg45_menu` WHERE `alias` LIKE "historic-%";
-DELETE FROM `cultibox_joomla`.`dkg45_redirect_links` WHERE `referer` LIKE "http://localhost:6891/cultibox/index.php/historic%";
-DELETE FROM `cultibox_joomla`.`dkg45_redirect_links` WHERE `old_url` LIKE "http://localhost:6891/cultibox/index.php/historic%";
