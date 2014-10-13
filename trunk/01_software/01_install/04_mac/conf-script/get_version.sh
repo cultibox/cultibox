38ecc619f3001b25da0103eceacd62430bebc043
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-10-13 12:51:13 +0000

    Correction des installateurs mac et linux

--- trunk/01_software/01_install/04_mac/conf-script/get_version.sh
+++ trunk/01_software/01_install/04_mac/conf-script/get_version.sh
@@ -4,19 +4,17 @@ set -e
 
 user_culti=`who|head -1|awk -F" " '{print $1}'`
 group_culti=`who|head -1|awk -F" " '{print $1}'|xargs id -gn`
-home=`egrep "^$user_culti" /etc/passwd|awk -F":" '{print $6}'`
 
 # Test of the connection:
 /Applications/cultibox/xamppfiles/bin/mysql --defaults-extra-file=/Applications/cultibox/xamppfiles/etc/my-extra.cnf -h 127.0.0.1 --port=3891 cultibox -e "SELECT * FROM  configuration;" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
         result=`/Applications/cultibox/xamppfiles/bin/mysql --defaults-extra-file=/Applications/cultibox/xamppfiles/etc/my-extra.cnf -h 127.0.0.1 --port=3891 --batch cultibox -e "SELECT VERSION FROM configuration;"`
         for res in `echo $result`; do
-            if [ "`echo $res|egrep [1-9].*\.[1-9].*\.[1-9].*-.*`" != "" ]; then
+            if [ "`echo $res|egrep [1-9].*\.[0-9].*\.[0-9].*-.*`" != "" ]; then
                 echo $res
                 exit 0
             fi
         done
-else
-        exit 1
 fi
-exit 0
+exit 1
+
