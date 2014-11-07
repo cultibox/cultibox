971ee6f7c159f9a6c084a31ef209dcbd5985bd3d
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-11-07 13:53:44 +0000

    Script de backup/load permet de sauvegarder et restaurer indépendamenet de la version (compat ascendante, pas descendante) -> mac + correctif linux

--- trunk/01_software/01_install/04_mac/conf-script/load.sh
+++ trunk/01_software/01_install/04_mac/conf-script/load.sh
@@ -10,7 +10,7 @@ echo "              Cultibox load database script                    "
 echo "-----------------------------------------------------------------"
 echo ""
 
-if [ -f $HOME/.cultibox/backup_cultibox.sql ]; then
+if [ -f $HOME/cultibox/backup_cultibox.sql ]; then
     # To load a previous database dump: deletion of the current database, creation of the new database, import of the previous dump.
     echo "  * Cultibox: import of your backup database..."
     # Test of the connection:
@@ -24,8 +24,35 @@ if [ -f $HOME/.cultibox/backup_cultibox.sql ]; then
                 exit 0
             fi
         done
-        echo "  * Loading $home/.cultibox/backup_cultibox.sql file..."
-        /Applications/cultibox/xamppfiles/bin/mysql --defaults-extra-file=/Applications/cultibox/xamppfiles/etc/my-extra.cnf -h 127.0.0.1 --port=3891 cultibox < $HOME/.cultibox/backup_cultibox.sql
+        echo "  * Loading $HOME/cultibox/backup_cultibox.sql file..."
+        /Applications/cultibox/xamppfiles/bin/mysql --defaults-extra-file=/Applications/cultibox/xamppfiles/etc/my-extra.cnf -h 127.0.0.1 --port=3891 cultibox < $HOME/cultibox/backup_cultibox.sql
+
+        version=`head -1 $HOME/cultibox/backup_cultibox.sql`
+        version=`echo $version|awk -F ": " '{print $2}'`
+        if [ "$version" != "" ]; then
+            version=`echo ${version%%-*}`
+            version=`echo ${version//./}`
+
+            if [ "$version" != "" ]; then
+                for file in `ls /Applications/cultibox/sql_install/update_sql-* 2>/dev/null`; do
+                    #Remove path file information
+                    file_name=`basename $file`
+                    #Remove extension information
+                    file_name=` echo ${file_name%.*}`
+                    #Get version information
+                    file_name=`echo $file_name|awk -F"-" '{print $2}'`
+                    file_name=`echo ${file_name//./}`
+
+                    if [ "$file_name" != "" ]; then
+                        #if file version >= version
+                        if [ $file_name -ge $version ]; then
+                            /Applications/cultibox/xamppfiles/bin/mysql --defaults-extra-file=/Applications/cultibox/xamppfiles/etc/my-extra.cnf -f -h 127.0.0.1 --port=3891 < $file
+                        fi
+                    fi
+                done
+            fi
+        fi
+        echo "... OK"
     else
         echo "===== Error accessing cultibox database, exiting... ===="
         echo "... NOK"
@@ -33,7 +60,7 @@ if [ -f $HOME/.cultibox/backup_cultibox.sql ]; then
     fi
     echo "... OK"
 else
-    echo "  * Missing $HOME/.cultibox/backup_cultibox.sql file..."
+    echo "  * Missing $HOME/cultibox/backup_cultibox.sql file..."
     echo "...NOK"
 fi
 exit 0
