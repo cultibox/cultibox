57578ed3f198e526f6fa07f4797c761ca075eaa0
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-11-13 10:34:11 +0000

    Clean fichiers upgrade sql + on force l'upgrade même en cas d'erreur

--- trunk/01_software/01_install/01_src/02_sql/update_sql-1.1.12.sql
+++
@@ -1,5 +0,0 @@
-SET CHARACTER SET utf8;
-
--- Version 1.1.12:
-ALTER TABLE `cultibox`.`configuration` DROP COLUMN `LANG`;
-
--- trunk/01_software/01_install/01_src/02_sql/update_sql-1.1.14.sql
+++
@@ -1,6 +0,0 @@
-SET CHARACTER SET utf8;
-
--- Version 1.1.14:
-ALTER TABLE `cultibox`.`power` ADD INDEX ( `timestamp` );
-ALTER TABLE `cultibox`.`logs` ADD INDEX ( `timestamp` );
-
