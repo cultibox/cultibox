e99f06909175b89f3d07e1c2e1a32e94ccceaf99
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2014-12-26 17:22:41 +0000

    Ajout du repertoire de conf cultipi

--- 01_software/02_src/cultibox/main/scripts/cultipi.php
+++ 01_software/02_src/cultibox/main/scripts/cultipi.php
@@ -7,18 +7,13 @@ $start_load = getmicrotime();
 $main_error=array();
 $main_info=array();
 
-// ================= VARIABLES ================= //
-$nb_plugs=get_configuration("NB_PLUGS",$main_error);
-$status=get_canal_status($main_error);
-$step=1;
-
 // Trying to find if a cultibox SD card is currently plugged and if it's the case, get the path to this SD card
 if((!isset($GLOBALS['MODE']))||(strcmp($GLOBALS['MODE'],"cultipi")!=0)) { 
     if((!isset($sd_card))||(empty($sd_card))) {
         $sd_card=get_sd_card();
     }
 } else {
-        $sd_card="/etc/cultipi";
+    $sd_card="/etc/cultipi/conf_tmp";
 }
 
 // By default the expanded menu is the user interface menu
@@ -30,28 +25,6 @@ if((!isset($submenu))||(empty($submenu))) {
 if((!isset($sd_card))||(empty($sd_card))) {
     setcookie("CHECK_SD", "False", time()+1800,"/",false,false);
 }
-
-$error_value[2]=__('ERROR_VALUE_PROGRAM','html');
-$error_value[3]=__('ERROR_VALUE_PROGRAM_TEMP','html');
-$error_value[4]=__('ERROR_VALUE_PROGRAM_HUMI','html');
-$error_value[5]=__('ERROR_VALUE_PROGRAM_CM','html');
-$error_value[6]=__('ERROR_VALUE_PROGRAM','html');
-
-// Setting some default values:
-if((empty($selected_plug))||(!isset($selected_plug))) {
-    $selected_plug=1;
-}
-
-$plug_name=get_plug_conf("PLUG_NAME",$selected_plug,$main_error);
-$plug_type=get_plug_conf("PLUG_TYPE",$selected_plug,$main_error);
-$plug_power_max=get_plug_conf("PLUG_POWER_MAX",$selected_plug,$main_error);
-
-$start_time="06:00:00";
-$end_time="18:00:00";
-
-if($selected_plug==1) {
-    $plug_type="lamp";
-} 
 
 //Compute time loading for debug option
 $end_load = getmicrotime();
