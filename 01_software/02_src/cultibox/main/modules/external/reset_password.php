bcbc5811b97aec0fd389c8b8e405b7078e75b3ae
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-01-23 15:00:37 +0000

    Mise en place du changement de mot de passe (testé)

--- 01_software/02_src/cultibox/main/modules/external/reset_password.php
+++ 01_software/02_src/cultibox/main/modules/external/reset_password.php
@@ -1,11 +1,21 @@
 <?php
 
 if((isset($_GET['pwd']))&&(!empty($_GET['pwd']))) {
-   $pwd=$_GET['pwd'];
-   exec("(echo -n 'cultipi:Identification:' && echo -n 'cultipi:Identification:test' | md5sum | awk '{print $1}') >> /tmp/.passwd",$output,$err);
-   if(is_file("/tmp/.passwd")) {
-       exec("sudo mv /tmp/.passwd /etc/lighttpd/",$output,$err);
-   }
+   $err=0;
+   exec("(echo -n 'cultipi:Identification:' && echo -n 'cultipi:Identification:".$_GET['pwd']."' | md5sum | awk '{print $1}') >> /tmp/.passwd",$output,$err);
+   if($err==0) {
+       if(is_file("/tmp/.passwd")) {
+           exec("sudo mv /tmp/.passwd /etc/lighttpd/",$output,$err);
+       } else {
+         $err=1;
+       }
+   } 
+}
+
+if($err==0) {
+    echo json_encode("1");
+} else {
+    echo json_encode("0");
 }
 
 ?>
