106b0de72dc477d77b9143e75a02e8cc0b2243ba
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-01-30 16:19:17 +0000

    Nettoyage cultinet

--- 01_software/02_src/cultibox/main/modules/external/get_soft_version.php
+++ 01_software/02_src/cultibox/main/modules/external/get_soft_version.php
@@ -4,7 +4,6 @@
     $err="";
 
     exec("dpkg -s cultipi|grep Version|awk -F \"Version: \" '{print $2}'",$ret[0],$err);
-    exec("dpkg -s cultinet|grep Version|awk -F \"Version: \" '{print $2}'",$ret[1],$err);
     exec("dpkg -s cultibox|grep Version|awk -F \"Version: \" '{print $2}'",$ret[2],$err);
     exec("dpkg -s cultiraz|grep Version|awk -F \"Version: \" '{print $2}'",$ret[3],$err);
     exec("dpkg -s cultitime|grep Version|awk -F \"Version: \" '{print $2}'",$ret[4],$err);
--- 04_CultiPi/01_Software/02_cultinet/main/libs/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// List of encryption supported by the wifi module:
-$GLOBALS['WIFI_KEY_TYPE_LIST']=array("NONE", "WEP", "WPA (TKIP + AES)","WPA (TKIP)","WPA (AES/CCMP)");
-
-?>
-
-
