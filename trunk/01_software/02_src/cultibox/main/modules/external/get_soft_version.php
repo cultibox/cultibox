568e8dd40711c925bb5681bdfe2928185838d4a0
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-01-30 18:06:46 +0000

    correction

--- trunk/01_software/02_src/cultibox/main/modules/external/get_soft_version.php
+++ trunk/01_software/02_src/cultibox/main/modules/external/get_soft_version.php
@@ -4,9 +4,9 @@
     $err="";
 
     exec("dpkg -s cultipi|grep Version|awk -F \"Version: \" '{print $2}'",$ret[0],$err);
-    exec("dpkg -s cultibox|grep Version|awk -F \"Version: \" '{print $2}'",$ret[2],$err);
-    exec("dpkg -s cultiraz|grep Version|awk -F \"Version: \" '{print $2}'",$ret[3],$err);
-    exec("dpkg -s cultitime|grep Version|awk -F \"Version: \" '{print $2}'",$ret[4],$err);
+    exec("dpkg -s cultibox|grep Version|awk -F \"Version: \" '{print $2}'",$ret[1],$err);
+    exec("dpkg -s cultiraz|grep Version|awk -F \"Version: \" '{print $2}'",$ret[2],$err);
+    exec("dpkg -s cultitime|grep Version|awk -F \"Version: \" '{print $2}'",$ret[3],$err);
     
     echo json_encode($ret);
  
