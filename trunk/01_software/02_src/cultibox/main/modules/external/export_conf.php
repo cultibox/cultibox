d0571ae8efe1e845cc86ea878822869fbc4e825f
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-02-02 18:00:59 +0000

    changement de fonctionnement

--- trunk/01_software/02_src/cultibox/main/modules/external/export_conf.php
+++ trunk/01_software/02_src/cultibox/main/modules/external/export_conf.php
@@ -1,25 +1,6 @@
 <?php
 
-require_once('../../libs/config.php');
-
-if(isset($GLOBALS['MODE']) && $GLOBALS['MODE'] == "cultipi") {
-    exec("/var/www/cultibox/run/backup.sh",$output,$err);
-else {
-    $os=php_uname('s');
-    //Retrieve SD path depending of the current OS:
-    switch($os) {
-        case 'Linux':
-            exec("/opt/cultibox/run/backup.sh",$output,$err);
-            break;
-        case 'Mac':
-        case 'Darwin':
-            exec("/Applications/cultibox/run/backup.sh",$output,$err);
-            break;
-        case 'Windows NT':
-            exec("C:\cultibox\run\backup.sh",$output,$err);
-            break;
-    }
-}
+exec("/var/www/cultibox/run/backup.sh",$output,$err);
 $file="../../../tmp/export/backup_cultibox.sql";
 
 if(is_file("$file")) {
