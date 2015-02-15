adaadc8fe1b85a6c076548fff35a4593fd6e4dda
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-02-15 16:36:42 +0000

    correction

--- trunk/04_CultiPi/01_Software/05_cultiRAZ/cultiRAZ.tcl
+++ trunk/04_CultiPi/01_Software/05_cultiRAZ/cultiRAZ.tcl
@@ -182,8 +182,8 @@ proc checkAndUpdate {} {
                 exec /sbin/ifconfig wlan0 down
                 exec /sbin/iwconfig wlan0 mode Ad-Hoc      
                 exec /sbin/ifconfig wlan0 up
-                exec /usr/sbin/invoke-rc.d networking force-reload
-                exec /etc/rc.local
+                after 2000 exec /usr/sbin/invoke-rc.d networking force-reload
+                after 2000 exec /etc/rc.local
             } msg]
             if {$RC != 0} {puts  "[clock format [clock seconds] -format "%b %d %H:%M:%S"] : cultiRAZ : Error during network configuration : $msg"}
 
