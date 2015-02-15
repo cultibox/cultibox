c07222390d1662a44045233be54247454e9dfa80
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-02-15 20:20:22 +0000

    Ajout message si pas d'accés à l'exterieur

--- 01_software/02_src/cultibox/main/modules/external/check_rpi_update.php
+++ 01_software/02_src/cultibox/main/modules/external/check_rpi_update.php
@@ -4,9 +4,14 @@ $output=array();
 $upgrade=array();
 $err="";
 
-exec("sudo apt-get update -o Dir::Etc::sourcelist=\"sources.list.d/cultibox.list\"  -o Dir::Etc::sourceparts=\"-\" -o APT::Get::List-Cleanup=\"0\"",$output,$err);
-exec("sudo apt-get -u upgrade --assume-no|grep cultibox",$upgrade,$err);
+exec("ping -c 4 8.8.8.8 >/dev/null",$output,$err);
 
-echo json_encode($upgrade);
+if($err==0) {
+    exec("sudo apt-get update -o Dir::Etc::sourcelist=\"sources.list.d/cultibox.list\"  -o Dir::Etc::sourceparts=\"-\" -o APT::Get::List-Cleanup=\"0\"",$output,$err);
+    exec("sudo apt-get -u upgrade --assume-no|grep cultibox",$upgrade,$err);
+    echo json_encode($upgrade);
+} else {
+    echo json_encode("1");
+}
 
 ?>
