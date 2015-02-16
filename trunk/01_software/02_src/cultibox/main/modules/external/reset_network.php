06fce24bd84943bce6e484165ef8835232424f5e
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-02-16 13:02:54 +0000

    test correction password adhoc

--- trunk/01_software/02_src/cultibox/main/modules/external/reset_network.php
+++ trunk/01_software/02_src/cultibox/main/modules/external/reset_network.php
@@ -3,6 +3,7 @@
 exec("sudo /bin/cp /etc/network/interfaces.BASE /etc/network/interfaces",$output,$err);
 exec("sudo /sbin/ifconfig wlan0 down",$output,$err);
 exec("sudo /sbin/iwconfig wlan0 mode Ad-Hoc",$output,$err);
+exec("sudo /sbin/iwconfig wlan0 key off",$output,$err);
 exec("sudo /sbin/ifconfig wlan0 up",$output,$err);
 exec("sudo /usr/sbin/invoke-rc.d networking force-reload",$output,$err);
 exec("sudo /etc/rc.local",$output,$err);
