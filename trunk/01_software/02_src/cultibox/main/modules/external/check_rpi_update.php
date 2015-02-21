f9f5343d39cd05f2f39f37a5ef501a05c44101d9
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-02-21 12:30:49 +0000

    accélération de la vérif des mises à jours

--- trunk/01_software/02_src/cultibox/main/modules/external/check_rpi_update.php
+++ trunk/01_software/02_src/cultibox/main/modules/external/check_rpi_update.php
@@ -7,11 +7,6 @@ $err="";
 exec("ping -c 4 8.8.8.8 >/dev/null",$output,$err);
 
 if($err==0) {
-    exec("sudo apt-get update -o Dir::Etc::sourcelist=\"sources.list.d/cultibox.list\"  -o Dir::Etc::sourceparts=\"-\" -o APT::Get::List-Cleanup=\"0\"",$output,$err);
-    if(is_file("/etc/sources.list.d/cultibox-dev.list")) {
-            exec("sudo apt-get update -o Dir::Etc::sourcelist=\"sources.list.d/cultibox-dev.list\"  -o Dir::Etc::sourceparts=\"-\" -o APT::Get::List-Cleanup=\"0\"",$output,$err);
-    }
-
     exec("sudo apt-get -u upgrade --assume-no|grep cultibox",$upgrade,$err);
     echo json_encode($upgrade);
 } else {
