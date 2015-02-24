5b531121d4ba8fae57179b0d24ccaf016fef67ca
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-02-24 10:29:52 +0000

    Nouvelle procédure pour repasser en mode adHoc sans redémarrer le module: on décharge/recharge le module wifi du kernel

--- 01_software/02_src/cultibox/main/modules/external/reset_network.php
+++ 01_software/02_src/cultibox/main/modules/external/reset_network.php
@@ -1,10 +1,10 @@
 <?php
 
 exec("sudo /bin/cp /etc/network/interfaces.BASE /etc/network/interfaces",$output,$err);
-exec("sudo /sbin/ifconfig wlan0 down",$output,$err);
-exec("sudo /sbin/iwconfig wlan0 mode Ad-Hoc",$output,$err);
-exec("sudo /sbin/ifconfig wlan0 up",$output,$err);
-exec("sudo /usr/sbin/invoke-rc.d networking force-reload",$output,$err);
+exec("sudo /sbin/modprobe -r rt2800usb",$output,$err);
+sleep(2);
+exec("sudo /sbin/modprobe rt2800usb",$output,$err);
+sleep(7);
 exec("sudo /etc/rc.local",$output,$err);
 exec("sudo /bin/mv /var/cache/lighttpd/compress/cultibox /tmp/",$output,$err);
 
