a04357418145449cd8d922009f541321442be9d8
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-02-24 18:51:16 +0000

    modification gestion des conf réseau

--- 01_software/02_src/cultibox/main/modules/external/reset_network.php
+++ 01_software/02_src/cultibox/main/modules/external/reset_network.php
@@ -5,7 +5,10 @@ exec("sudo /sbin/modprobe -r rt2800usb",$output,$err);
 sleep(2);
 exec("sudo /sbin/modprobe rt2800usb",$output,$err);
 sleep(7);
+exec("sudo /usr/sbin/invoke-rc.d networking force-reload",$output,$err);
+sleep(2);
 exec("sudo /etc/rc.local",$output,$err);
 exec("sudo /bin/mv /var/cache/lighttpd/compress/cultibox /tmp/",$output,$err);
+sleep(7);
 
 ?>
