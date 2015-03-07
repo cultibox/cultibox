25ccb92c7de9dec07006025a4920a8098c5f511e
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-03-07 11:45:13 +0000

    correction du reset du réseau avec la nouvelle version

--- trunk/01_software/02_src/cultibox/main/modules/external/reset_network.php
+++ trunk/01_software/02_src/cultibox/main/modules/external/reset_network.php
@@ -7,7 +7,6 @@ exec("sudo /sbin/modprobe rt2800usb",$output,$err);
 sleep(7);
 exec("sudo /usr/sbin/invoke-rc.d networking force-reload",$output,$err);
 sleep(2);
-exec("sudo /etc/rc.local",$output,$err);
 exec("sudo /bin/mv /var/cache/lighttpd/compress/cultibox /tmp/",$output,$err);
 sleep(7);
 
