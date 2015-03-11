fe0ccd8264ff4f3033588686dcf90debbd8166fd
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-03-11 15:44:48 +0000

    Correction compare_conf + ajout de la version de l'image soft

--- trunk/01_software/02_src/cultibox/main/modules/external/compare_conf.php
+++ trunk/01_software/02_src/cultibox/main/modules/external/compare_conf.php
@@ -9,7 +9,7 @@
     $current_plg_conf   = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi/serverPlugUpdate/plg";
 
     $tmp_prg_conf       = $GLOBALS['CULTIPI_CONF_TEMP_PATH'] . "/cnf/prg";
-    $current_prg_conf   = $GLOBALS['CULTIPI_CONF_PATH'] . "01_defaultConf_RPi/serverPlugUpdate/prg";
+    $current_prg_conf   = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi/serverPlugUpdate/prg";
 
     exec("diff -r $tmp_plg_conf $current_plg_conf",$ret,$err);
 
