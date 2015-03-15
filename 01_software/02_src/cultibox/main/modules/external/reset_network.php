aaaeca729c2913a904c3a630e4493ce5de71cbfc
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-03-15 14:18:35 +0000

    correction

--- 01_software/02_src/cultibox/main/modules/external/reset_network.php
+++ 01_software/02_src/cultibox/main/modules/external/reset_network.php
@@ -4,7 +4,6 @@ exec("sudo /bin/cp /etc/network/interfaces.BASE /etc/network/interfaces",$output
 exec("sudo /sbin/modprobe -r rt2800usb",$output,$err);
 sleep(2);
 exec("sudo /sbin/modprobe rt2800usb",$output,$err);
-exec("sudo /bin/mv /var/lib/dhcp/dhclient.*.leases",$output,$err);
 sleep(7);
 exec("sudo /usr/sbin/invoke-rc.d networking force-reload",$output,$err);
 sleep(2);
