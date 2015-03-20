0d09033cd946cc53888aa200c2810073ac924475
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-03-20 15:39:04 +0000

    nettoyage

--- branches/02_src/cultibox/main/modules/external/create_configuration.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    require_once('../../libs/utilfunc.php');
-
-    $return=create_network_file($_GET);
-    echo json_encode($return);
-
-?>
--- branches/02_src/cultibox/main/modules/external/upgrade_rpi.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$err="";
-$output=array();
-exec("sudo apt-get install -y  --force-yes --only-upgrade -o Dpkg::Options::=\"--force-confdef\" -o Dpkg::Options::=\"--force-confold\" cultibox cultitime cultiraz culticonf cultipi",$output,$err);
-
-
-?>
