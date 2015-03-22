2b0d3c015baa6e28678a0d492fe032e6150514eb
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-03-22 22:51:22 +0000

    correction gestion conf

--- 01_software/02_src/cultibox/main/modules/external/compare_conf.php
+++ 01_software/02_src/cultibox/main/modules/external/compare_conf.php
@@ -5,19 +5,10 @@
     $ret = array();
     $err="";
 
-    $tmp_plg_conf       = $GLOBALS['CULTIPI_CONF_TEMP_PATH'] . "/cnf/plg";
-    $current_plg_conf   = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi/serverPlugUpdate/plg";
+    $tmp_conf       = $GLOBALS['CULTIPI_CONF_TEMP_PATH'] . "/serverPlugUpdate";
+    $current_conf   = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi/serverPlugUpdate";
 
-    $tmp_prg_conf       = $GLOBALS['CULTIPI_CONF_TEMP_PATH'] . "/cnf/prg";
-    $current_prg_conf   = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi/serverPlugUpdate/prg";
+    exec("diff -r $tmp_conf $current_conf",$ret,$err);
 
-    exec("diff -r $tmp_plg_conf $current_plg_conf",$ret,$err);
-
-    if($err!=0) {
-        echo json_encode($err);
-    } else {
-        exec("diff -r $tmp_prg_conf $current_prg_conf",$ret,$err);
-        echo json_encode($err);
-    }
- 
+    echo json_encode($err);
 ?>
--- 01_software/02_src/cultibox/main/modules/external/sync_conf.php
+++ 01_software/02_src/cultibox/main/modules/external/sync_conf.php
@@ -2,15 +2,14 @@
 
     require_once('../../libs/config.php');
 
-    $path_ouput             = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi";
-    $path_ouput_linux_rm    = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi/*";
-    $path_ouput_linux_cp    = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi/";
+    $path_output             = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi";
+    $path_output_linux       = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi/*";
     $path_input_linux_cp    = $GLOBALS['CULTIPI_CONF_TEMP_PATH'] . "/*";
     
     switch(php_uname('s')) {
         case 'Windows NT':
             $path = str_replace("/","\\",$GLOBALS['CULTIPI_CONF_TEMP_PATH']);
-            $confPath = str_replace("/","\\",$path_ouput);
+            $confPath = str_replace("/","\\",$path_output);
             exec("xcopy /Y /E $path $confPath",$ret,$err);
             if ($err != 0) 
             {
@@ -18,16 +17,12 @@
             }
             break;
         default : 
-            exec("sudo rm -rf -R $path_ouput_linux",$ret,$err);
-            if ($err != 0) 
-            {
-                echo json_encode($err);
-            }
-            break;            
-            exec("sudo cp -R $path_input_linux_cp $path_ouput_linux_cp",$ret,$err);
-            if ($err != 0) 
-            {
-                echo json_encode($err);
+            if(($path_output!="")&&(is_dir($path_output))) {
+                exec("sudo cp -R $path_input_linux_cp $path_output/",$ret,$err);
+                if ($err != 0) 
+                {
+                    echo json_encode($err);
+                }
             }
             break;
     }
@@ -36,7 +31,7 @@
         case 'Windows NT':
             break;
         default : 
-            exec("sudo chown -R cultipi:cultipi $current_conf",$ret,$err);
+            exec("sudo chown -R cultipi:cultipi $path_output",$ret,$err);
 
             //Restart service:
             exec("sudo /etc/init.d/cultipi force-reload >/dev/null",$ret,$err);
