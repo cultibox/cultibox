2e40d6aa635a9e21723ef02d59e3cb11ae8571eb
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-03-26 09:51:44 +0000

    affichage des noms des prises dans l'onglet prise et correction du comparatif de la conf

--- trunk/01_software/02_src/cultibox/main/modules/external/compare_conf.php
+++ trunk/01_software/02_src/cultibox/main/modules/external/compare_conf.php
@@ -5,8 +5,8 @@
     $ret = array();
     $err="";
 
-    $tmp_conf       = $GLOBALS['CULTIPI_CONF_TEMP_PATH'] . "/serverPlugUpdate";
-    $current_conf   = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi/serverPlugUpdate";
+    $tmp_conf       = $GLOBALS['CULTIPI_CONF_TEMP_PATH'];
+    $current_conf   = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi";
 
     exec("diff -r $tmp_conf $current_conf",$ret,$err);
 
