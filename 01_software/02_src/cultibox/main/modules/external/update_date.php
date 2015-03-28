7678fb8f77ee1ad23c8e9a7d40d751dc9105d53e
alliaume.rico@gmail.com <alliaume.rico@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-03-28 18:54:44 +0000

    Mise à jour de l'heure du RTC Correctif de mise en forme page cultipi

--- 01_software/02_src/cultibox/main/modules/external/update_date.php
+++ 01_software/02_src/cultibox/main/modules/external/update_date.php
@@ -1,12 +1,29 @@
 <?php
 
-if((isset($_GET['date']))&&(!empty($_GET['date']))) {
-    if(strpos($_SERVER['REMOTE_ADDR'],"10.0.0.")!==false) {
+if(isset($_GET['date']) && !empty($_GET['date'])) {
+    if(strpos($_SERVER['REMOTE_ADDR'],"10.0.0.") !== false) {
+    
+        // Read actual hour
         exec("date +%s",$output,$err);
 
         //If we can't get the current timestamp or date is not set (date before 2014), we set the date:
-        if((count($output)!=1)||($output[0]<1388530000)) {
-            exec("sudo /bin/date -s @".$_GET['date'],$output,$err);
+        if(count($output)!=1 || $output[0]<1388530000)
+        {
+
+            // Update RPi hour using client hour
+            try {
+                $ret = exec("sudo /bin/date -s @".$_GET['date'],$output,$err);
+            } catch (Exception $e) {
+                echo 'Exception reçue : ',  $e->getMessage(), "\n";
+            }
+
+            // Update RTC Hour using RPi hour
+            try {
+                $ret = exec('tclsh "/opt/cultiTime/setDate.tcl"');
+            } catch (Exception $e) {
+                echo 'Exception reçue : ',  $e->getMessage(), "\n";
+            }
+
             echo "set date";
         }
     }
