7efb80b41d1adb78198f060d7903a9a39f742839
alliaume.rico@gmail.com <alliaume.rico@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-03-31 10:04:37 +0000

    Ajout des parametres demandés

--- 04_CultiPi/01_Software/09_cultiCam/cultiCam.tcl
+++ 04_CultiPi/01_Software/09_cultiCam/cultiCam.tcl
@@ -27,6 +27,8 @@ array set configXML {
     dailySnapshotHour       12
     dailySnapshotMin        0
     nbWebcam                0
+    lock_snapshotFile       /var/lock/culticam_snapshot
+    lock_reloadConfFile     /var/lock/culticam_reloadConf
     
 }
 set RC [catch {
@@ -39,6 +41,43 @@ puts "[clock format [clock seconds] -format "%Y %b %d %H:%M:%S"] : cultiCam : XM
 
 # On prend une photo toutes les X secondes
 proc takePhoto {webcamIndex} {
+
+    # On la sauvegarde dans les photos journalière une fois par jour
+    set Hour [string trimleft [clock format [clock seconds] -format "%H"] 0]
+    if {$Hour == ""} {set Hour 0}
+    set Minute [string trimleft [clock format [clock seconds] -format "%M"] 0]
+    if {$Minute == ""} {set Minute 0}
+    set date [string trimleft [clock format [clock seconds] -format "%y%m%d"] 0]
+    if {$Hour > $::configXML(dailySnapshotHour) && 
+        $Minute > $::configXML(dailySnapshotMin) && 
+        $::dailyPhotoIsTake != $date} {
+        
+        # On prend un image
+        set RC [catch {
+            exec sudo fswebcam -c  $::configXML(confPathWebcam,${webcamIndex})
+        } msg]
+        if {$RC != 0} {
+            puts "[clock format [clock seconds] -format "%Y %b %d %H:%M:%S"] : cultiCam : Error during taking snapshot webcam ${webcamIndex} , error : $msg"
+        }
+        
+        set RC [catch {
+            file copy -force /var/www/cultibox/tmp/webcam_${webcamIndex}_temp.jpg  /var/www/cultibox/tmp/webcam_${webcamIndex}_${date}.jpg 
+        } msg]
+        if {$RC != 0} {
+            puts "[clock format [clock seconds] -format "%Y %b %d %H:%M:%S"] : cultiCam : Error during saving daily snapshot webcam ${webcamIndex} , error : $msg"
+        }
+        
+        
+        set ::dailyPhotoIsTake ${date}
+    }
+
+
+    if {[file exists $::configXML(lock_snapshotFile)] != 1} {
+    
+        # Le fichier n'est pas présent, on ne fait donc pas de snapshot
+        after 1000 takePhoto $webcamIndex
+        
+    }
 
     puts "[clock format [clock seconds] -format "%Y %b %d %H:%M:%S"] : cultiCam : Take Snapshot webcam ${webcamIndex}" ; update
 
@@ -62,37 +101,34 @@ proc takePhoto {webcamIndex} {
     #if {$RC != 0} {
     #    puts "[clock format [clock seconds] -format "%Y %b %d %H:%M:%S"] : cultiCam : Error during copying snapshot webcam ${webcamIndex} , error : $msg"
     #}
-    
-    
-    # On la sauvegarde dans les photos journalière une fois par jour
-    set Hour [string trimleft [clock format [clock seconds] -format "%H"] 0]
-    if {$Hour == ""} {set Hour 0}
-    set Minute [string trimleft [clock format [clock seconds] -format "%M"] 0]
-    if {$Minute == ""} {set Minute 0}
-    set date [string trimleft [clock format [clock seconds] -format "%y%m%d"] 0]
-    if {$Hour > $::configXML(dailySnapshotHour) && 
-        $Minute > $::configXML(dailySnapshotMin) && 
-        $::dailyPhotoIsTake != $date} {
-        
-        set RC [catch {
-            file copy -force /var/www/cultibox/tmp/webcam_${webcamIndex}_temp.jpg  /var/www/cultibox/tmp/webcam_${webcamIndex}_${date}.jpg 
-        } msg]
-        if {$RC != 0} {
-            puts "[clock format [clock seconds] -format "%Y %b %d %H:%M:%S"] : cultiCam : Error during saving daily snapshot webcam ${webcamIndex} , error : $msg"
-        }
-        
-        
-        set ::dailyPhotoIsTake ${date}
-    }
 
-    after [expr 1000 * $::configXML(timeBeforeTwoSnapshots)] takePhoto $webcamIndex
+    after 20 takePhoto $webcamIndex
 }
 
 # Pour chaque webcam, on lance la boucle d'acquisition
 for {set i 0} {$i < $::configXML(nbWebcam)} {incr i} {
-    after [expr 1000 * $i] takePhoto $i
+    takePhoto $i
 }
 
+# On vérifie régulièrement s'il faut recharger le fichier de conf 
+proc reloadXML {} {
+
+    if {[file exists $::configXML(lock_reloadConfFile)] == 1} {
+        puts "[clock format [clock seconds] -format "%Y %b %d %H:%M:%S"] : cultiCam : reloadXML: Reload conf file"
+        array set ::configXML [::piXML::convertXMLToArray $::confXML]
+        
+        set RC [catch {
+            file delete -force  $::configXML(lock_reloadConfFile)
+        } msg]
+        if {$RC != 0} {
+            puts "[clock format [clock seconds] -format "%Y %b %d %H:%M:%S"] : cultiCam : reloadXML : Error during deleting lock file (path : $::configXML(lock_reloadConfFile)) , error : $msg"
+        }
+        
+    }
+
+    after 250 reloadXML
+}
+reloadXML
 
 # On attend indéfiniment
 vwait forever
