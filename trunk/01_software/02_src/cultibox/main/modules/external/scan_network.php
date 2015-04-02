bc8824f0b25ca381794141a2d381ab8d0ff54d5f
yann.genevois@gmail.com <yann.genevois@gmail.com@5a18a202-1113-9b45-73dc-adc674c8ea78>
2015-04-02 15:37:16 +0000

    Correction webcam + passage réseau ouvert pour la configuration

--- trunk/01_software/02_src/cultibox/main/modules/external/scan_network.php
+++ trunk/01_software/02_src/cultibox/main/modules/external/scan_network.php
@@ -2,10 +2,13 @@
 
 $wifi_list=array();
 
-exec("sudo /sbin/iwlist wlan0 scan |/bin/grep ESSID|/usr/bin/awk -F \"\\\"\" '{print $2}'",$wifi_net_list,$error);
-foreach($wifi_net_list as $list) {
-    if((strpos("$list","cultipi_")!==0)&&(strcmp("$list","")!=0)) {
-        $wifi_list[]=$list;
+exec("sudo /usr/sbin/iw wlan0 scan ap-force|/bin/grep SSID|/usr/bin/awk -F\": \" '{print $2}'",$wifi_net_list,$error);
+$wifi_list=array_unique($wifi_net_list);
+
+
+foreach($wifi_list as $list) {
+    if((strpos(trim($list),"cultipi_")!=0)&&(strcmp(str_replace(" ","",trim($list)),"")!=0)) {
+        $wifi_list[]=trim($list);
     }
 }
 echo json_encode($wifi_list);
